6b68f2b6c0be0fabaadfb221309adecf724f1e19
momentiris <momentiris@gmail.com>
2019-07-11 15:00:23 +0200

    feat: init kubernetes

---
+++ k8s/README.md
@@ -0,0 +1,1 @@
+## 1. Reconfigure the aws-ebs-driver by applying the aws-ebs-driver.yaml to the kubernetes cluster
--- packages/match-route/lib/config.js
+++ packages/match-route/lib/config.js
@@ -3,10 +3,15 @@ const config = require('@iteam/config')({
   defaults: {
     port: 3030,
     osrm: 'http://localhost:5000',
+    redis: {
+      host: '127.0.0.1',
+      port: 6379,
+    },
   },
 })
 
 module.exports = {
   port: config.get('port'),
   osrm: config.get('osrm'),
+  redis: config.get('redis'),
 }

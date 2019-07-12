a41ca8a42a3fbc195a6bd3735e79d4ddb4852d84
Radu Achim <radu.achim@iteam.se>
2019-07-12 17:54:04 +0200

    sh or bash are they not same thing?

--- bin/build-condition.sh
+++ bin/build-condition.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ -z $1 ]]; then
     echo "Commit range cannot be empty"
--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 DOCKER_REPO="iteam1337/predictive-movement-${1/packages\//}";

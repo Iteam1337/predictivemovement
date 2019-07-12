0de8d95d95524ffe82a00a845984aaca4ad8f519
Radu Achim <radu.achim@iteam.se>
2019-07-12 17:47:34 +0200

    ci: 🎡 docker build

---
+++ bin/build.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+
+DOCKER_REPO="iteam1337/predictive-movement-${1/packages\//}";
+DOCKER_TAG="latest"
+PROJECT=$1
+
+echo "building $DOCKER_REPO:$DOCKER_TAG"
+
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
+
+docker build -t $DOCKER_REPO:$DOCKER_TAG --build-arg "GOOGLE_API_TOKEN=$GOOGLE_API_TOKEN" \
+  --build-arg "MAPBOX_TOKEN=$MAPBOX_TOKEN" \
+  --build-arg "API_HOST=$API_HOST" $PROJECT
+
+docker push $DOCKER_REPO:$DOCKER_TAG
+
+docker logout

2abfdecf3ac7599c906350bb4845832a709da87b
Radu Achim <radu.achim@iteam.se>
2019-07-17 20:45:21 +0200

    ci: 🎡 replace special characters in git branch

--- bin/build-feature.sh
+++ bin/build-feature.sh
@@ -2,17 +2,13 @@
 
 
 DOCKER_REPO="iteam1337/predictive-movement-${1/packages\//}";
-DOCKER_TAG="feat/some-feature"
+DOCKER_TAG=$(echo $TRAVIS_PULL_REQUEST_BRANCH | sed 's/\+/-/g; s/\//_/g; s/\=//g')
 PROJECT=$1
-
 echo "building $DOCKER_REPO:$DOCKER_TAG"
-echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
-echo "TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
 
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 
 docker build -t $DOCKER_REPO:$DOCKER_TAG $PROJECT
-
 docker push $DOCKER_REPO:$DOCKER_TAG
 
 docker logout

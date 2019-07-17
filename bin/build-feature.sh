848715dbf1b16a2b21a0e60a087040fd054a0bb0
Radu Achim <radu.achim@iteam.se>
2019-07-17 16:50:10 +0200

    ci: 🎡 test hardcoded docker tag

--- bin/build-feature.sh
+++ bin/build-feature.sh
@@ -2,19 +2,17 @@
 
 
 DOCKER_REPO="iteam1337/predictive-movement-${1/packages\//}";
-DOCKER_TAG="latest"
+DOCKER_TAG="feat/some-feature"
 PROJECT=$1
 
 echo "building $DOCKER_REPO:$DOCKER_TAG"
 echo "TRAVIS_BRANCH is $TRAVIS_BRANCH"
 echo "TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
 
-# docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 
-# docker build -t $DOCKER_REPO:$DOCKER_TAG --build-arg "GOOGLE_API_TOKEN=$GOOGLE_API_TOKEN" \
-#   --build-arg "MAPBOX_TOKEN=$MAPBOX_TOKEN" \
-#   --build-arg "API_HOST=$API_HOST" $PROJECT
+docker build -t $DOCKER_REPO:$DOCKER_TAG $PROJECT
 
-# docker push $DOCKER_REPO:$DOCKER_TAG
+docker push $DOCKER_REPO:$DOCKER_TAG
 
-# docker logout
+docker logout

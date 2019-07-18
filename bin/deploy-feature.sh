c3bebb05acb160a73e46cce398f6616a81ed1ac3
Radu Achim <radu.achim@iteam.se>
2019-07-18 11:27:48 +0200

    chore: why is bash so difficult

--- bin/deploy-feature.sh
+++ bin/deploy-feature.sh
@@ -18,6 +18,7 @@ helm template k8s/charts/$DEPLOYMENT --name $DEPLOYMENT --namespace $FEATURE \
   --set image.tag=$FEATURE | \
 kubectl --server=$KUBERNETES_SERVER --token=$KUBERNETES_TOKEN --insecure-skip-tls-verify=true apply -f -
 
+### Copy google and mapbox secrets from default namespace into the new namespace
 kubectl --server=$KUBERNETES_SERVER --token=$KUBERNETES_TOKEN --insecure-skip-tls-verify=true get secret google-token -n default -o yaml | \
   sed "s/namespace: default/namespace: $FEATURE/" | \
   kubectl --server=$KUBERNETES_SERVER --token=$KUBERNETES_TOKEN --insecure-skip-tls-verify=true apply -n $FEATURE -f -
@@ -26,9 +27,10 @@ kubectl --server=$KUBERNETES_SERVER --token=$KUBERNETES_TOKEN --insecure-skip-tl
   sed "s/namespace: default/namespace: $FEATURE/" | \
   kubectl --server=$KUBERNETES_SERVER --token=$KUBERNETES_TOKEN --insecure-skip-tls-verify=true apply -n $FEATURE -f -
 
-if ! (kubectl --server=$KUBERNETES_SERVER --token=$KUBERNETES_TOKEN -n $FEATURE get pod -l app.kubernetes.io/name=$DEPLOYMENT -o jsonpath='{.items[0].metadata.name}'); \
-  then \
-    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Deployment preview ready at: https://$DEPLOYMENT-$FEATURE.pm.iteamdev.se\"}" \
-    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"; \
-  fi
+### Post a comment to Github PR when there isn't a pod yet for the new feature (prevents from posting on every commit)
+if ! (kubectl --server=$KUBERNETES_SERVER --token=$KUBERNETES_TOKEN -n $FEATURE get pod -l app.kubernetes.io/name=$DEPLOYMENT -o jsonpath='{.items[0].metadata.name}');
+then
+  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+  -d "{\"body\": \"Deployment preview ready at: https://$DEPLOYMENT-$FEATURE.pm.iteamdev.se\"}" \
+  "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments";
+fi

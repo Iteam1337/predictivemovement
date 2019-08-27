e6cb4d3e8ba44839f3fa72eba8b27c9c1d2c98d4
Dennis Pettersson <dennis.pettersson@iteam.se>
2019-08-27 20:23:17 +0200

    feat: allow one generate at a time

--- packages/plot-routes/src/routes/generate.ts
+++ packages/plot-routes/src/routes/generate.ts
@@ -2,7 +2,15 @@ import { RequestHandler } from 'express'
 import genRequests from '../services/genRequests'
 import hasProp from '../util/hasProp'
 
+let running = false
+
 export const handler: RequestHandler = async ({ query }, res) => {
+  if (running) {
+    return res.status(409).send({ status: 'generate in progress' })
+  }
+
+  running = true
+
   let destinationJSON: any
   try {
     destinationJSON = JSON.parse(
@@ -11,52 +19,46 @@ export const handler: RequestHandler = async ({ query }, res) => {
   } catch (_) {
     //
   }
-
   const wait = hasProp(query, 'wait') && query.wait === 'true'
-
-  const destination =
-    destinationJSON &&
-    hasProp(destinationJSON, 'lat') &&
-    hasProp(destinationJSON, 'lon') &&
-    typeof destinationJSON.lat === 'number' &&
-    typeof destinationJSON.lon === 'number'
-      ? destinationJSON
-      : undefined
 
   const count =
     hasProp(query, 'count') &&
     !isNaN(Number.parseFloat(query.count)) &&
     Number.parseInt(query.count, 10)
-    
   const radiusInKm =
     hasProp(query, 'radiusInKm') &&
     !isNaN(Number.parseFloat(query.radiusInKm)) &&
     Number.parseInt(query.radiusInKm, 10)
 
   const params = {
-    destination,
+    destination:  destinationJSON &&
+    hasProp(destinationJSON, 'lat') &&
+    hasProp(destinationJSON, 'lon') &&
+    typeof destinationJSON.lat === 'number' &&
+    typeof destinationJSON.lon === 'number'
+      ? destinationJSON
+      : undefined,
     radiusInKm:
       typeof radiusInKm === 'number'
-        ? Math.max(10, Math.min(radiusInKm, 100))
+        ? Math.max(1, Math.min(radiusInKm, 200))
         : undefined,
     count:
-      typeof count === 'number'
-        ? Math.max(1, Math.min(count, 256))
-        : undefined,
+      typeof count === 'number' ? Math.max(1, Math.min(count, 256)) : undefined,
   }
-  
-  
-  console.log({ params })
 
   if (!wait) {
     res.status(200).send(null)
-    await genRequests(params)
+
+    await genRequests(params).then(() => {
+      running = false
+    })
+
     return
   }
 
-  const response = await genRequests(params)
+  res.status(200).json(await genRequests(params))
 
-  res.status(200).json(response)
+  running = false
 }
 
 export default handler

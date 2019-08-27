a25d56aebcac7dbffca806a25f1ab5c18566d1e1
Dennis Pettersson <dennis.pettersson@iteam.se>
2019-08-27 21:57:25 +0200

    feat: reason?

--- packages/match-route/lib/routes/demo.js
+++ packages/match-route/lib/routes/demo.js
@@ -1,44 +1,59 @@
 const routeStore = require('../services/routeStore')
 const hasProp = require('../utils/hasProp')
 
-const reasonCheckForNullSoThatThingsDontBreak = object =>
-  object
-    && hasProp(object, 'distance')
-    && hasProp(object, 'duration')
-    && hasProp(object, 'stops')
-    && hasProp(object, 'maxTime')
-    && Array.isArray(object.stops)
-    // && (object.stops.length ? (hasProp(object.stops[0], 'lat') && hasProp(object.stops[0], 'lng')) : true)
+const keepStructure = object => {
+  const data = Object.values(object).filter(
+    object =>
+      object != null &&
+      hasProp(object, 'distance') &&
+      hasProp(object, 'duration') &&
+      hasProp(object, 'stops') &&
+      hasProp(object, 'maxTime') &&
+      Array.isArray(object.stops)
+  )
+
+  data.forEach(data => {
+    data.stops = data.stops.filter(
+      stop => hasProp(stop, 'lat') && hasProp(stop, 'lon')
+    )
+  })
+
+  return data
+}
 
 module.exports = app => {
   app.get('/demo/pending', async (_, res) => {
-    const dump = await routeStore.pending.dump()
     res.send({
-      data: Object.values(dump).filter(Boolean).filter(reasonCheckForNullSoThatThingsDontBreak)
+      data: keepStructure(await routeStore.pending.dump()),
     })
   })
 
   app.get('/demo/pending/locked', async (_, res) => {
-    const dump = await routeStore.pending.dump()
-    res.send(dump.filter(pending => pending.locked))
+    res.send(
+      (await routeStore.pending.dump()).filter(
+        pending => pending.locked != null
+      )
+    )
   })
 
   app.get('/demo/pending/unlocked', async (_, res) => {
-    const dump = await routeStore.pending.dump()
-    res.send(Object.values(dump.filter(pending => !pending.locked)))
+    res.send(
+      Object.values(
+        (await routeStore.pending.dump()).filter(
+          pending => pending.locked == null
+        )
+      )
+    )
   })
 
   app.get('/demo/routes', async (_, res) => {
-    const dump = await routeStore.routes.dump()
-
     res.send({
-      data: Object.values(dump).filter(Boolean).filter(reasonCheckForNullSoThatThingsDontBreak),
+      data: keepStructure(await routeStore.routes.dump()),
     })
   })
 
   app.get('/demo/persons', async (_, res) => {
-    const dump = await routeStore.persons.dump()
-    res.send(dump)
+    res.send(await routeStore.persons.dump())
   })
 
   app.get('/demo/clear', async (_, res) => {

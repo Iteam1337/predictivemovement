b3ab24389bb1ef2ce538d9bfb329a4adf9c66cd0
Radu Achim <radu.achim@iteam.se>
2020-03-30 18:32:09 +0200

    refactor: remove unused code from dispatch evaluate

--- packages/engine-elixir/lib/dispatch.ex
+++ packages/engine-elixir/lib/dispatch.ex
@@ -1,23 +1,5 @@
 defmodule Dispatch do
-  def evaluate(bookings, cars) do
-    bookings
-    |> Enum.reduce(%{cars: cars, assignments: []}, fn booking, result ->
-      [candidate | _rest] = CarFinder.find(booking, result.cars)
-
-      bestCar = Car.assign(candidate.car, candidate.booking, :auto)
-
-      newCars =
-        result.cars
-        |> Enum.map(fn car ->
-          if car.id == bestCar.id, do: bestCar, else: car
-        end)
-
-      %{
-        cars: newCars,
-        assignments:
-          result.assignments ++ [%{booking: booking, car: bestCar, score: candidate.score}]
-      }
-    end)
-    |> Score.calculateTotalScore()
+  def evaluate(booking, car) do
+    # TODO: filter based on if the car is full, booking has restrictions etc.
   end
 end

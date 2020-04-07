cbcc8f0916d3115e02259bb70a594e25edb90bac
Radu Achim <radu.achim@iteam.se>
2020-04-07 14:19:25 +0200

    chore: supervisor for car simulator
    Co-authored-by: Mikael <intemicke@gmail.com>

--- packages/car-simulator/lib/car_simulator.ex
+++ packages/car-simulator/lib/car_simulator.ex
@@ -1,16 +1,25 @@
-defmodule CarSimulator do
-  use Application
+defmodule CarSimulator.App do
+  use GenServer, restart: :permanent
+
   @center %{lat: 61.829182, lon: 16.0896213}
 
-  def start(_type, _args) do
-    children = []
+  def start_link(opts \\ [name: __MODULE__]) do
+    GenServer.start_link(__MODULE__, nil, opts)
+  end
+
+  def init(_opts) do
+    start_simulation()
+    {:ok, nil}
+  end
+
+  # Public interface
+  def start_simulation() do
+    IO.puts("started simulation")
 
     Cars.simulate(@center, 5)
     |> Stream.map(fn car ->
       MQ.publish(Application.fetch_env!(:car_simulator, :exchange), car)
     end)
     |> Stream.run()
-
-    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end

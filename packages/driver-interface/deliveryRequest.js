70ee111a53d2050adc1bdb119bc9348d335f03af
Izabella Larsson <izalar2001@skola.goteborg.se>
2020-04-14 17:20:00 +0200

    feat: telegram happy path (#41)
    * feat: move out booking bot from driver-interface into booking-interface
    
    Co-authored-by: Mikael <intemicke@gmail.com>
    Co-authored-by: Andreas Lundqvist <momentiris@gmail.com>
    Co-authored-by: irony <christian@landgren.nu>
    Co-authored-by: lundeeell <lundell3@gmail.com>
    
    * chore: refactor deliveryRequest
    
    Co-authored-by: lundeeell <lundell3@gmail.com>
    Co-authored-by: Andreas Lundqvist <momentiris@gmail.com>
    
    * chore: add googelGeo to get adresses and refactor deliveryRequest
    
    Co-authored-by: lundeeell <lundell3@gmail.com>
    Co-authored-by: Andreas Lundqvist <momentiris@gmail.com>
    
    Co-authored-by: Mikael <intemicke@gmail.com>
    Co-authored-by: Andreas Lundqvist <momentiris@gmail.com>
    Co-authored-by: irony <christian@landgren.nu>
    Co-authored-by: lundeeell <lundell3@gmail.com>

--- packages/driver-interface/deliveryRequest.js
+++ packages/driver-interface/deliveryRequest.js
@@ -3,8 +3,6 @@ const open = require('amqplib').connect('amqp://localhost')
 const Markup = require('telegraf/markup')
 
 const deliveryRequest = (chatId, msgOptions, { car, booking }) => {
-  console.log('msgOptions', car)
-  console.log('msgOptions', booking)
   bot.telegram.sendMessage(
     chatId,
     `Ett paket finns att hämta på Munkebäcksgatan 33F som ska levereras till Storhöjdsgatan 9, har du möjlighet att hämta detta?`,
@@ -38,25 +36,91 @@ const deliveryRequest = (chatId, msgOptions, { car, booking }) => {
   )
 }
 
-bot.action('confirm', (ctx) => {
-  console.log(ctx)
-  ctx.reply('yay')
-})
+const onPickupConfirm = (msg) => {
+  const chatId = msg.update.callback_query.from.id
+  bot.telegram.sendMessage(
+    chatId,
+    `Härligt, nu kan du köra paketet till <insert en destination>`,
+    {
+      parse_mode: 'markdown',
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Levererat',
+              callback_data: 'delivered',
+            },
+          ],
+        ],
+      },
+    }
+  )
+}
+
+const onPackageDelivered = () => {
+  console.log('package has been delivered')
+}
+
+const pickupInstructions = () => {
+  return open
+    .then((conn) => conn.createChannel())
+    .then((ch) =>
+      ch
+        .assertQueue('pickupInstructions', {
+          durable: false,
+        })
+
+        .then(
+          () =>
+            new Promise((resolve) => {
+              ch.consume('pickupInstructions', (msg) => {
+                const message = JSON.parse(msg.content.toString())
+                ch.ack(msg)
+                resolve(message)
+              })
+            })
+        )
+        .then((instructions) => {
+          bot.telegram.sendMessage(
+            instructions.id,
+            `Bra du ska nu åka hit [Starta GPS](https://www.google.com/maps/dir/?api=1&&destination=${instructions.booking.departure.lat},${instructions.booking.departure.lon})`,
+            {
+              parse_mode: 'markdown',
+              reply_markup: {
+                inline_keyboard: [
+                  [
+                    {
+                      text: 'Hämtat',
+                      callback_data: 'confirm',
+                    },
+                  ],
+                ],
+              },
+            }
+          )
+        })
+    )
+}
 
 bot.on('callback_query', (msg) => {
-  let isAccepted
-  let options
+  if (msg.update.callback_query.data === 'confirm') {
+    return onPickupConfirm(msg)
+  }
 
   try {
-    const { a, ...opt } = JSON.parse(msg.update.callback_query.data)
-    isAccepted = a
-    options = opt
+    const { a: isAccepted, ...options } = JSON.parse(
+      msg.update.callback_query.data
+    )
+
+    if (options && options.r && options.id) {
+      onDeliveryRequestResponse(isAccepted, options, msg)
+    }
   } catch (error) {
     return
   }
+})
 
-  if (!options || !options.r || !options.id) return
-
+const onDeliveryRequestResponse = (isAccepted, options, msg) => {
   msg.editMessageReplyMarkup()
   msg.answerCbQuery()
   msg.reply(isAccepted ? 'Kul!' : 'Tråkigt!')
@@ -67,30 +131,71 @@ bot.on('callback_query', (msg) => {
       ch.sendToQueue(options.r, Buffer.from(isAccepted.toString()), {
         correlationId: options.id,
       })
-      return ch
     })
-    .then(
-      (ch) =>
-        new Promise((resolve) =>
-          ch.consume('pickup_instructions', (msg) => {
-            ch.ack(msg)
-            resolve(msg)
-          })
-        )
-    )
-    .then((data) => JSON.parse(data.content.toString()))
-    .then((instructions) => {
-      console.log('received instructions', instructions)
-      msg.replyWithMarkdown(
-        `Bra du ska nu åka hit [Starta GPS](https://www.google.com/maps/dir/?api=1&&destination=${instructions.booking.departure.lat},${instructions.booking.departure.lon})`,
-        Markup.inlineKeyboard([
-          Markup.callbackButton('Hämtat', 'confirm'),
-        ]).extra()
-      )
-    })
+
     .catch(console.warn)
-})
+}
 
 module.exports = {
   deliveryRequest,
+  pickupInstructions,
 }
+
+// Pickup instructions example message
+// { "id": "1124095220",
+//   "booking":{
+//      "departure": {"latitude": "57.00234", "longitude": "16.0134143" }}}
+
+// Delivery RPC example message
+// {
+//   "car": {
+//     "route": {
+//       "weight_name": "routability",
+//       "weight": 1561,
+//       "started": "2020-04-07T13:09:42.334878",
+//       "legs": [
+//         null
+//       ],
+//       "geometry": [
+//         null
+//       ],
+//       "duration": 1561,
+//       "distance": 18171
+//     },
+//     "position": {
+//       "lon": 11.801236,
+//       "lat": 57.736512
+//     },
+//     "instructions": [
+//       [
+//         null
+//       ],
+//       [
+//         null
+//       ]
+//     ],
+//     "id": 1124095220,
+//     "heading": {
+//       "position": [
+//         null
+//       ],
+//       "booking": [
+//         null
+//       ],
+//       "action": "pickup"
+//     },
+//     "busy": false
+//   },
+//   "booking": {
+//     "id": 493,
+//     "destination": {
+//       "lon": 16.143512,
+//       "lat": 61.905908
+//     },
+//     "departure": {
+//       "lon": 15.981676,
+//       "lat": 61.847311
+//     },
+//     "bookingDate": "2020-04-07T13:09:15.732840Z"
+//   }
+// }

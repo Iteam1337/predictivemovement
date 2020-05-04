1d6888931f1673c8b841c5e0e0d342f87d6989f0
Radu Achim <radu.achim@iteam.se>
2020-05-04 15:28:13 +0200

    refactor: make bookingRequests like routes

--- packages/engine-elixir/lib/bookingRequests.ex
+++ packages/engine-elixir/lib/bookingRequests.ex
@@ -1,27 +1,30 @@
 defmodule BookingRequests do
   def init do
-    parent = self()
-
     Stream.resource(
-      fn ->
-        spawn(fn ->
-          queue_name = "booking_requests"
-          # Application.fetch_env!(:engine, :bookings_exchange)
-          bookings_exchange = "bookings"
-          {:ok, connection} = AMQP.Connection.open(Application.fetch_env!(:engine, :amqp_host))
-          {:ok, channel} = AMQP.Channel.open(connection)
-          AMQP.Exchange.declare(channel, bookings_exchange, :topic)
-          AMQP.Queue.declare(channel, queue_name)
-          AMQP.Queue.bind(channel, queue_name, bookings_exchange, routing_key: "new")
-
-          AMQP.Queue.subscribe(channel, queue_name, fn booking, _meta ->
-            send(parent, {:msg, booking: booking |> Poison.decode!(%{keys: :atoms})})
-          end)
-        end)
-      end,
+      fn -> subscribe(self()) end,
       fn pid -> receive_next_value(pid) end,
       fn values -> values end
     )
+  end
+
+  defp decode(booking) do
+    booking |> Poison.decode!(%{keys: :atoms})
+  end
+
+  defp subscribe(parent) do
+    spawn(fn ->
+      exchange = "bookings"
+      queue = "booking_requests"
+      {:ok, connection} = AMQP.Connection.open(Application.fetch_env!(:engine, :amqp_host))
+      {:ok, channel} = AMQP.Channel.open(connection)
+      AMQP.Exchange.declare(channel, exchange, :topic)
+      AMQP.Queue.declare(channel, queue)
+      AMQP.Queue.bind(channel, queue, exchange, routing_key: "new")
+
+      AMQP.Queue.subscribe(channel, queue, fn booking, _meta ->
+        send(parent, {:msg, booking: decode(booking)})
+      end)
+    end)
   end
 
   defp receive_next_value(pid) do

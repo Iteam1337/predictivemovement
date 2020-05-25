7f699c3acc02acf5b075db2b824385bc4796fb39
momentiris <momentiris@gmail.com>
2020-05-26 00:20:01 +0200

    refactor: state and routing

--- packages/engine-ui/src/index.tsx
+++ packages/engine-ui/src/index.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.js'
 import * as serviceWorker from './serviceWorker'
 import { SocketIOProvider } from 'use-socketio'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <SocketIOProvider
     url={process.env.REACT_APP_ENGINE_SERVER || 'http://localhost:4000'}
   >
-    <App />
+    <Router>
+      <App />
+    </Router>
   </SocketIOProvider>,
   document.getElementById('root')
 )

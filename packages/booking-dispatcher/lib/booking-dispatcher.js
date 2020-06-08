8c04bcfa94d165f3e41f6302f867cade1109dcba
izabellalarsson <izzwii.izabella@gmail.com>
2020-06-08 11:41:26 +0200

    feat: loop over each past booking and send to rabbitMQ

--- packages/booking-dispatcher/lib/booking-dispatcher.js
+++ packages/booking-dispatcher/lib/booking-dispatcher.js
@@ -5,6 +5,10 @@ const XLSX = require('xlsx')
 const format = require('date-fns/format')
 const setYear = require('date-fns/setYear')
 const fetch = require('node-fetch')
+const amqp = require('amqplib').connect(
+  process.env.AMQP_HOST || 'amqp://localhost'
+)
+const id62 = require('id62').default
 
 // from telegram booking
 // {
@@ -14,7 +18,7 @@ const fetch = require('node-fetch')
 //   departure: { lon: 12.123993, lat: 58.120939 },
 //   destination: { lon: 12.016714, lat: 57.719823 }
 const wait = (time) =>
-new Promise(resolve => setTimeout(() => resolve(), time))
+  new Promise((resolve) => setTimeout(() => resolve(), time))
 
 const bookingDispatcher = async () => {
   if (!process.env.file) {
@@ -25,7 +29,6 @@ const bookingDispatcher = async () => {
   const buf = fs.readFileSync(`${process.cwd()}/data/${process.env.file}`)
   const wb = XLSX.read(buf, { type: 'buffer' })
 
- 
   const debugDate = new Date('2020-09-13')
 
   const packages = XLSX.utils
@@ -33,39 +36,75 @@ const bookingDispatcher = async () => {
     .filter(
       (x) =>
         format(setYear(new Date(x.ShipmentDate), 2020), 'yyyy-MM-dd') ===
-        format(debugDate, 'yyyy-MM-dd'),
+        format(debugDate, 'yyyy-MM-dd')
     )
 
+  // const changedData = {departure: {lon: 57.7213783, lat: 12.0173562}, destination: {lon: 57.7213783, lat: 12.1173562}, id: id62(), bookingDate: withGeoCodes.ShipmentDate, ...withGeoCodes}
+  //   // withGeoCodes['Till Postnummer'] = {departure: {lon: 57.7213783, lat: 12.0173562}}
 
-    // const changedData = {departure: {lon: 57.7213783, lat: 12.0173562}, destination: {lon: 57.7213783, lat: 12.1173562}, id: id62(), bookingDate: withGeoCodes.ShipmentDate, ...withGeoCodes}
-    //   // withGeoCodes['Till Postnummer'] = {departure: {lon: 57.7213783, lat: 12.0173562}}
+  // const withGeoCodes = packages.map(async (p) => {
+  //   const to = await fetch(
+  //     `https://nominatim.openstreetmap.org/search?country=sweden&postalcode=${p['Till Postnummer']}&format=json`,
+  //   ).then((res) => res.json())
 
-    // const withGeoCodes = packages.map(async (p) => {
-    //   const to = await fetch(
-    //     `https://nominatim.openstreetmap.org/search?country=sweden&postalcode=${p['Till Postnummer']}&format=json`,
-    //   ).then((res) => res.json())
-  
-    //   const from = await fetch(
-    //     `https://nominatim.openstreetmap.org/search?country=sweden&postalcode=${p['Från Postnummer']}&format=json`,
-    //   ).then((res) => res.json())
-    // }) 
-    
-    const fetchGeoCodes = async(postalcode) => {
-      const geoCode = await fetch(`https://nominatim.openstreetmap.org/search?country=sweden&postalcode=${postalcode}&format=json`).then((res => res.json()))
-      console.log(geoCode)
-    }
+  //   const from = await fetch(
+  //     `https://nominatim.openstreetmap.org/search?country=sweden&postalcode=${p['Från Postnummer']}&format=json`,
+  //   ).then((res) => res.json())
+  // })
 
-
-
-    for (let package of packages) {
-      // console.log('to', package['Till Postnummer'])
-      await fetchGeoCodes('41671');
-      await wait(5000);
-      // console.log('from', package['Från Postnummer'])
-      // await wait(5000)
+  const fetchGeoCodes = async (postalcode) => {
+    const geoCode = await fetch(
+      `https://nominatim.openstreetmap.org/search?country=sweden&postalcode=${postalcode}&format=json`
+    )
+      .then((res) => res.json())
+      .catch((error) => console.log(error))
+    return geoCode
   }
 
+  for (let package of packages) {
+    // console.log('to', package['Till Postnummer'])
+    const to = await fetchGeoCodes(package['Till Postnummer'])
+    bookingTo = {
+      departure: { lon: parseFloat(to[0].lon), lat: parseFloat(to[0].lat) },
+    }
+    await wait(5000)
 
+    const from = await fetchGeoCodes(package['Från Postnummer'])
+
+    await wait(5000)
+    package = {
+      destination: {
+        lon: parseFloat(from[0].lon),
+        lat: parseFloat(from[0].lat),
+      },
+      id: id62(),
+      senderId: 'the-past',
+      bookingDate: package.ShipmentDate,
+      ...bookingTo,
+    }
+
+    createBooking(package)
+  }
+}
+
+const createBooking = (booking) => {
+  console.log(booking)
+  return amqp
+    .then((conn) => conn.createChannel())
+    .then((ch) =>
+      ch
+        .assertExchange('bookings', 'topic', {
+          durable: false,
+        })
+        .then(() =>
+          ch.publish(
+            'bookings',
+            'new',
+            Buffer.from(JSON.stringify({ ...booking, assigned_to: null }))
+          )
+        )
+    )
+    .catch(console.warn)
 }
 
 bookingDispatcher()

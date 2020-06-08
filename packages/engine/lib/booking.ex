696942c582e083eca7158af96e7b8fb15bcc1b13
Radu Achim <radu.achim@iteam.se>
2020-06-08 13:26:20 +0200

    chore: remove unused vehicle functions

--- packages/engine/lib/booking.ex
+++ packages/engine/lib/booking.ex
@@ -1,10 +1,6 @@
 defmodule Booking do
   defstruct [:id, :pickup, :delivery, :assigned_to, :senderId]
 
-  def calculate_score(%Vehicle{} = vehicle, %Booking{} = booking) do
-    Vehicle.get_score_diff_with_new_booking(vehicle, booking)
-  end
-
   def assign(%Booking{} = booking) do
     MQ.publish(
       booking,
--- packages/engine/lib/vehicle.ex
+++ packages/engine/lib/vehicle.ex
@@ -11,65 +11,6 @@ defmodule Vehicle do
     %Vehicle{id: id, position: position, busy: busy}
   end
 
-  def get_score_diff_with_new_booking(
-        %Vehicle{instructions: [], position: vehicle_position} = vehicle,
-        %Booking{
-          pickup: pickup,
-          delivery: delivery
-        }
-      ),
-      do:
-        {Map.put(vehicle, :instructions, [pickup, delivery]),
-         get_osrm_distance([vehicle_position, pickup, delivery, vehicle_position])}
-
-  def get_score_diff_with_new_booking(
-        %Vehicle{instructions: old_instructions, position: _vehicle_position} = vehicle,
-        %Booking{pickup: pickup, delivery: delivery}
-      ) do
-    old_score = get_osrm_distance(old_instructions)
-
-    {new_instructions, new_score} =
-      old_instructions
-      |> get_possible_route_permutations(pickup, delivery)
-      |> Enum.map(fn new_instructions ->
-        {new_instructions, get_osrm_distance(new_instructions)}
-      end)
-      |> Enum.sort_by(fn {_, score} -> score end, :asc)
-      |> List.first()
-
-    {Map.put(vehicle, :instructions, new_instructions), old_score - new_score}
-  end
-
-  def get_possible_route_permutations(instructions, pickup, delivery) do
-    instructions
-    |> get_possible_indices_for_new_instruction()
-    |> Enum.map(fn {pickup_index, delivery_index} ->
-      instructions
-      |> List.insert_at(pickup_index, pickup)
-      |> List.insert_at(delivery_index, delivery)
-    end)
-  end
-
-  defp get_possible_indices_for_new_instruction(list_length),
-    do: do_get_indices(0, 1, length(list_length) + 2)
-
-  defp do_get_indices(pickup_index, _, list_length) when pickup_index >= list_length,
-    do: []
-
-  defp do_get_indices(pickup_index, delivery_index, list_length)
-       when delivery_index == list_length,
-       do: do_get_indices(pickup_index + 1, pickup_index + 2, list_length)
-
-  defp do_get_indices(pickup_index, delivery_index, list_length),
-    do:
-      do_get_indices(pickup_index, delivery_index + 1, list_length)
-      |> Enum.concat([{pickup_index, delivery_index}])
-
-  def get_osrm_distance(instructions) do
-    Osrm.route(instructions)
-    |> Map.get(:distance)
-  end
-
   def offer(%Vehicle{id: id, instructions: instructions, booking_ids: booking_ids} = vehicle) do
     IO.inspect(vehicle, label: "offer to vehicle")
     instructions_without_start = Enum.filter(instructions, &Map.has_key?(&1, :id))

e8c95421d643a443cd1aa2580eaaee48ca2b6161
izabellalarsson <izzwii.izabella@gmail.com>
2020-06-15 17:03:47 +0200

    fix: import function

--- packages/driver-interface/services/secondsToHm.js
+++ packages/driver-interface/services/secondsToHm.js
@@ -4,7 +4,7 @@ const secondsToHm = (seconds) => {
   const m = Math.floor((seconds % 3600) / 60)
 
   const hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : ''
-  const mDisplay = m > 0 ? m + (m == 1 ? ' minut, ' : ' minuter, ') : ''
+  const mDisplay = m > 0 ? m + (m == 1 ? ' minut' : ' minuter') : ''
   return hDisplay + mDisplay
 }
 

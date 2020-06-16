d3edef4d45ca9e2a53e21b0f24fc7b1bf837dc13
Simon Lundell <simon.lundell@iteam.se>
2020-06-16 14:07:17 +0200

    feat: randomize address from excel

---
+++ packages/booking-dispatcher/lib/adapters/google.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch')
+
+const fetchGeoCodes = (postalcode) =>
+  fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=sweden,${postalcode}&key=${process.env.GOOGLE_API_TOKEN}`
+  )
+    .then((res) => res.json())
+    .then(({ results }) => results[0].geometry.location)
+    .then(({ lat, lng: lon }) => ({ coordinates: { lat, lon } }))
+
+module.exports = {
+  fetchGeoCodes,
+}

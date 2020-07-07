5b5dba97c8dc100456defc2ee3f141c96ad173b8
Mikael Gråborg <mikael.graborg@iteam.se>
2020-07-07 17:26:07 +0200

    chore: add hints before sending to jsprit

--- packages/engine/lib/plan.ex
+++ packages/engine/lib/plan.ex
@@ -8,6 +8,7 @@ defmodule Plan do
     |> Map.put(:vehicles, Enum.map(vehicle_ids, &Vehicle.get/1))
     |> Map.put(:bookings, Enum.map(booking_ids, &Booking.get/1))
     |> Plan.insert_time_matrix()
+    |> IO.inspect(label: "this is sent to jsprit")
     |> MQ.call("route_optimization_jsprit")
     |> Poison.decode!(keys: :atoms)
   end
@@ -21,5 +22,39 @@ defmodule Plan do
       |> Map.delete(:code)
 
     Map.put(items, :matrix, matrix)
+    |> add_hints_from_matrix()
+  end
+
+  def add_hints_from_matrix(%{bookings: bookings} = map) do
+    {booking_hints, vehicle_hints} =
+      map
+      |> get_in([:matrix, :sources])
+      |> Enum.map(fn %{hint: hint} -> hint end)
+      |> Enum.split(length(bookings) * 2)
+
+    map
+    |> Map.update!(:bookings, fn bookings -> add_booking_hints(bookings, booking_hints) end)
+    |> Map.update!(:vehicles, fn vehicles -> add_vehicle_hints(vehicles, vehicle_hints) end)
+  end
+
+  defp add_booking_hints(bookings, hints) do
+    bookings
+    |> Enum.reduce({[], hints}, fn booking, {res, [pickup_hint, delivery_hint | rest_of_hints]} ->
+      updated_booking =
+        booking
+        |> Map.update!(:pickup, fn position -> Map.put(position, :hint, pickup_hint) end)
+        |> Map.update!(:delivery, fn position -> Map.put(position, :hint, delivery_hint) end)
+
+      {res ++ [updated_booking], rest_of_hints}
+    end)
+    |> elem(0)
+  end
+
+  def add_vehicle_hints(vehicles, hints) do
+    vehicles
+    |> Enum.zip(hints)
+    |> Enum.map(fn {vehicle, hint} ->
+      Map.update!(vehicle, :position, fn position -> Map.put(position, :hint, hint) end)
+    end)
   end
 end

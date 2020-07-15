88f8bcd547b3d0365fc2df8487e757dc2f088722
Mikael Gråborg <mikael.graborg@iteam.se>
2020-07-15 15:27:22 +0200

    refactor: tidy up match_producer

--- packages/engine/lib/match_producer.ex
+++ packages/engine/lib/match_producer.ex
@@ -15,38 +15,36 @@ defmodule Engine.MatchProducer do
     {:producer, %{vehicles: [], bookings: []}}
   end
 
-  def handle_info({:basic_consume_ok, _}, state), do: {:noreply, [], state}
-
-  def handle_info(
-        {:basic_deliver, vehicle, %{exchange: @incoming_vehicle_exchange}},
-        %{
-          vehicles: vehicles,
-          bookings: bookings
-        }
-      ) do
-    IO.puts("new vehicle!")
-    vehicle = string_to_vehicle_transform(vehicle)
-
-    dispatch_events([vehicle | vehicles], bookings)
-  end
-
-  def handle_info(
-        {:basic_deliver, booking, %{exchange: @incoming_booking_exchange}},
-        %{
-          vehicles: vehicles,
-          bookings: bookings
-        }
-      ) do
+  defp handle_new_booking(booking_msg, %{vehicles: vehicles, bookings: bookings}) do
     IO.puts("new booking!")
-    booking = string_to_booking_transform(booking)
-    IO.inspect(bookings, label: "current bookings state")
-    dispatch_events(vehicles, [booking | bookings])
+    new_booking = string_to_booking_transform(booking_msg)
+    dispatch_events(vehicles, [new_booking | bookings])
   end
 
-  def handle_info(
-        {:basic_deliver, _, %{routing_key: @clear_queue}},
-        _
-      ) do
+  defp handle_new_vehicle(vehicle_msg, %{vehicles: vehicles, bookings: bookings}) do
+    IO.puts("new vehicle!")
+    new_vehicle = string_to_vehicle_transform(vehicle_msg)
+    dispatch_events([new_vehicle | vehicles], bookings)
+  end
+
+  ## helpers
+
+  defp string_to_vehicle_transform(vehicle_string) do
+    %{position: position, metadata: metadata} =
+      vehicle_string |> Poison.decode!(keys: :atoms) |> Map.put_new(:metadata, %{})
+
+    Vehicle.make(position, metadata)
+  end
+
+  defp string_to_booking_transform(booking_string) do
+    %{pickup: pickup, delivery: delivery, id: external_id, metadata: metadata} =
+      Poison.decode!(booking_string, keys: :atoms)
+      |> Map.put_new(:metadata, %{})
+
+    Booking.make(pickup, delivery, external_id, metadata)
+  end
+
+  def handle_clear_state do
     IO.puts("Clearing MatchProducer state")
     {:noreply, [], %{vehicles: [], bookings: []}}
   end
@@ -69,6 +67,17 @@ defmodule Engine.MatchProducer do
     {:noreply, [message], %{bookings: bookings, vehicles: vehicles}}
   end
 
+  # Rabbitmq callbacks
+  def handle_info({:basic_consume_ok, _}, state), do: {:noreply, [], state}
+
+  def handle_info({:basic_deliver, vehicle, %{exchange: @incoming_vehicle_exchange}}, state),
+    do: handle_new_vehicle(vehicle, state)
+
+  def handle_info({:basic_deliver, booking, %{exchange: @incoming_booking_exchange}}, state),
+    do: handle_new_booking(booking, state)
+
+  def handle_info({:basic_deliver, _, %{routing_key: @clear_queue}}, _), do: handle_clear_state()
+
   ## handle backpressure
 
   def handle_demand(_demand, state) do
@@ -77,22 +86,5 @@ defmodule Engine.MatchProducer do
 
   def ack(_ack_ref, _successful, _failed) do
     :ok
-  end
-
-  ## helpers
-
-  def string_to_vehicle_transform(vehicle_string) do
-    %{position: position, metadata: metadata} =
-      vehicle_string |> Poison.decode!(keys: :atoms) |> Map.put_new(:metadata, %{})
-
-    Vehicle.make(position, metadata)
-  end
-
-  def string_to_booking_transform(booking_string) do
-    %{pickup: pickup, delivery: delivery, id: external_id, metadata: metadata} =
-      Poison.decode!(booking_string, keys: :atoms)
-      |> Map.put_new(:metadata, %{})
-
-    Booking.make(pickup, delivery, external_id, metadata)
   end
 end

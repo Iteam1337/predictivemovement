cccce636ac6d2f1be2fb37bcca16ed34d3663e94
Mikael Gråborg <mikael.graborg@iteam.se>
2020-08-31 13:57:57 +0200

    chore: clean up merge

--- packages/route-optimization-jsprit/src/main/java/com/predictivemovement/route/optimization/VRPSetting.java
+++ packages/route-optimization-jsprit/src/main/java/com/predictivemovement/route/optimization/VRPSetting.java
@@ -45,40 +45,5 @@ public class VRPSetting {
         float latitude = jsonLocation.getFloat("lat");
         Location location = Location.newInstance(longitude, latitude);
         return location;
-    }<<<<<<<HEAD
-
-    private int cubicMetersToCentimeter(int meters) {
-        return meters * 1000 * 1000;
     }
-
-    private VehicleType getVehicleType(JSONObject vehicle) {
-        String profile = vehicle.optString("profile");
-        JSONArray capacities = vehicle.optJSONArray("capacity");
-        String vehicleId = profile != null ? profile : "vehicleDummyType";
-        VehicleTypeImpl.Builder vehicleTypeBuilder = VehicleTypeImpl.Builder.newInstance(vehicleId);
-        int volume = capacities != null ? cubicMetersToCentimeter(capacities.getInt(0)) : VEHICLE_DEFAULT_VOLUME;
-        int weight = capacities != null ? capacities.getInt(1) : VEHICLE_DEFAULT_WEIGHT;
-        vehicleTypeBuilder.addCapacityDimension(VOLUME_INDEX, volume);
-        vehicleTypeBuilder.addCapacityDimension(WEIGHT_INDEX, weight);
-        vehicleTypeBuilder.setCostPerDistance(1);
-        vehicleTypeBuilder.setCostPerTransportTime(1);
-        return vehicleTypeBuilder.build();
-    }
-
-    private int getVolumeFromMeasurements(JSONArray measurements) {
-        if (measurements == null)
-            return SHIPMENT_DEFAULT_VOLUME;
-        int volume = 1;
-
-        for (int i = 0; i < measurements.length(); i++) {
-            int measurement = measurements.getInt(i);
-            volume *= measurement;
-        }
-
-        return volume;
-    }
-
-    private void createCostMatrix() {
-        routingCosts = new VRPCostMatrix().set(routeRequest, locations);
-    }=======>>>>>>>27 c7c1bee9bea55e0f4763b53abc8d73d9e6b4c9
 }

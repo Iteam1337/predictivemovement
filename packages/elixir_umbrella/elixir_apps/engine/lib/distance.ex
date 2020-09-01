713bde830a2e4d72e0c3a08e2a0ad00855385c88
Radu Achim <radu.achim@iteam.se>
2020-09-01 17:35:10 +0200

    chore: shared distance calculation

--- packages/elixir_umbrella/elixir_apps/engine/lib/distance.ex
+++ packages/elixir_umbrella/elixir_apps/engine/lib/distance.ex
@@ -1,8 +1,38 @@
 defmodule Distance do
-  def calculate_distance(activities) do
-    activities
-    |> Enum.reduce(%{shared: 0}, fn %{distance: distance, id: id}, acc ->
-      acc |> Map.put(String.to_atom(id), (Map.get(acc, String.to_atom(id)) || 0) + distance)
+  def calculate_distance([start_route | rest]) do
+    [end_route | booking_activities] = Enum.reverse(rest)
+
+    number_of_bookings =
+      booking_activities
+      |> Enum.group_by(fn %{id: id} -> id end)
+      |> Map.keys()
+      |> length
+
+    calculate_distance(
+      booking_activities,
+      (start_route.distance + end_route.distance) / number_of_bookings
+    )
+  end
+
+  def calculate_distance(booking_activities, shared_cost_per_booking) do
+    booking_activities
+    |> Enum.reduce(%{}, fn %{
+                             distance: distance,
+                             id: id
+                           },
+                           acc ->
+      acc
+      |> Map.put(
+        String.to_atom(id),
+        (Map.get(acc, String.to_atom(id)) || 0) + distance
+      )
+    end)
+    |> Enum.reduce(%{}, fn {id, specific}, acc ->
+      acc
+      |> Map.put(
+        id,
+        %{specific: specific, shared: shared_cost_per_booking}
+      )
     end)
   end
 end
--- packages/elixir_umbrella/elixir_apps/engine/test/distance_test.exs
+++ packages/elixir_umbrella/elixir_apps/engine/test/distance_test.exs
@@ -2,10 +2,10 @@
 # Start --100--> Pickup A --300--> Deliver A --200--> End
 # Result: Total: 600 ?, A: 100 + 300 ?
 # - Scenario 2:
-# Start --100--> Pickup A --300--> Deliver A --100--> Pickup B --200--> Deliver B --100--> End
+# Start --100--> Pickup A --300--> Deliver A --100--> Pickup B --200--> Deliver B --200--> End
 # Result: Total: 800 ?, A: 400 ?, B: 300 or 700 ?
 # - Scenario 3:
-# Start --100--> Pickup A --200--> Pickup B  --100--> Deliver A --200--> Deliver B --100--> End
+# Start --100--> Pickup A --200--> Pickup B  --100--> Deliver A --200--> Deliver B --200--> End
 # Result: I don't even know
 
 defmodule DistanceTest do
@@ -13,7 +13,7 @@ defmodule DistanceTest do
 
   @pickupA %{
     distance: 100,
-    id: "A",
+    id: "start",
     type: "pickupShipment"
   }
 
@@ -23,8 +23,20 @@ defmodule DistanceTest do
     type: "deliverShipment"
   }
 
+  @pickupB %{
+    distance: 100,
+    id: "B",
+    type: "pickupShipment"
+  }
+
+  @deliverB %{
+    distance: 200,
+    id: "B",
+    type: "deliverShipment"
+  }
+
   @endRoute %{
-    id: "A",
+    id: "end",
     distance: 200,
     type: "end"
   }
@@ -33,7 +45,15 @@ defmodule DistanceTest do
   test "1 booking gets all the cost" do
     activities = [@pickupA, @deliverA, @endRoute]
     result = Distance.calculate_distance(activities)
-    expected = %{A: 600, shared: 0}
+    expected = %{A: %{specific: 300, shared: 300}}
+    assert result == expected
+  end
+
+  @tag :only
+  test "2 booking share no costs" do
+    activities = [@pickupA, @deliverA, @pickupB, @deliverB, @endRoute]
+    result = Distance.calculate_distance(activities)
+    expected = %{A: %{specific: 300, shared: 150}, B: %{specific: 300, shared: 150}}
     assert result == expected
   end
 end

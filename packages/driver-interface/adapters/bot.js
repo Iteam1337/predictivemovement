13433053f865d61578fbfc6f8aaaccb4e4a219ef
izabellalarsson <izzwii.izabella@gmail.com>
2020-09-07 16:27:55 +0200

    refactor: 💡 add more description to function name

--- packages/driver-interface/adapters/bot.js
+++ packages/driver-interface/adapters/bot.js
@@ -5,8 +5,8 @@ const idMap = new Map()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.context.metadata = {
-  getId: (telegramId) => idMap.get(telegramId),
-  setId: (telegramId, id) => idMap.set(telegramId, id),
+  getVehicleIdFromTelegramId: (telegramId) => idMap.get(telegramId),
+  setVehicleIdFromTelegramId: (telegramId, id) => idMap.set(telegramId, id),
 }
 
 module.exports = bot

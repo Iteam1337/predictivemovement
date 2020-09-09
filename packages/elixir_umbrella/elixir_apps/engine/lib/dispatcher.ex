d6913b61308fed35f8ced934fdda4324b1cb7c03
Mikael Gråborg <mikael.graborg@iteam.se>
2020-09-09 11:25:45 +0200

    chore: revert since init with empty vehicles array
    Since init with the empty vehicles solves the bug of
    infinite loop in the dispatcher, remove the double safety

--- packages/elixir_umbrella/elixir_apps/engine/lib/dispatcher.ex
+++ packages/elixir_umbrella/elixir_apps/engine/lib/dispatcher.ex
@@ -1,17 +1,12 @@
 defmodule Dispatcher do
   require Logger
 
-  defp offer_to_vehicles(vehicles) when length(vehicles) == 0, do: IO.puts("No vehicles to offer a plan to")
-
-  defp offer_to_vehicles(vehicles) do
-    Enum.map(vehicles, &Vehicle.offer/1)
-  end
-
   def dispatch_offers() do
     IO.puts("Dispatching!")
 
-    %{ vehicles: vehicles } = PlanStore.get_plan()
+    %{vehicles: vehicles} = PlanStore.get_plan()
     Logger.debug(length(vehicles), label: "amount of vehicles to be offered")
-    offer_to_vehicles(vehicles)
+
+    Enum.map(vehicles, &Vehicle.offer/1)
   end
 end

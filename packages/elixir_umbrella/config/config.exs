2304185b188ba9ef9c49a91a1869f9349dc92bc5
Mikael Gråborg <mikael.graborg@iteam.se>
2020-09-16 10:49:19 +0200

    chore: remove amqp logs but set gen level: info

--- packages/elixir_umbrella/config/config.exs
+++ packages/elixir_umbrella/config/config.exs
@@ -13,5 +13,6 @@ config :engine, :outgoing_plan_exchange, "outgoing_plan_updates"
 
 config :engine, :init_from_storage, System.get_env("INIT_FROM_STORAGE") == "true"
 
-config :logger, level: :warn
+config :logger, level: :info
+config :logger, handle_otp_reports: false
 import_config "#{Mix.env()}.exs"
--- packages/elixir_umbrella/elixir_apps/engine/mix.exs
+++ packages/elixir_umbrella/elixir_apps/engine/mix.exs
@@ -33,7 +33,7 @@ defmodule Engine.MixProject do
   defp deps do
     [
       {:message_generator, in_umbrella: true, only: :test},
-      {:amqp, "~> 1.0"},
+      {:amqp, "~> 1.5"},
       {:flow, "~> 1.0.0"},
       {:broadway, "~> 0.6.0"},
       {:broadway_rabbitmq, "~> 0.6.0"},

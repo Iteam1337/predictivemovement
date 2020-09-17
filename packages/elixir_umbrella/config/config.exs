2765c63ad50a46f5139e84eb8cdcaf7c07873668
Mikael Gråborg <mikael.graborg@iteam.se>
2020-09-17 11:33:30 +0200

    chore: make init from storage a run time config

--- packages/elixir_umbrella/config/config.exs
+++ packages/elixir_umbrella/config/config.exs
@@ -11,8 +11,6 @@ config :engine, :outgoing_booking_exchange, "outgoing_booking_updates"
 config :engine, :incoming_booking_exchange, "incoming_booking_updates"
 config :engine, :outgoing_plan_exchange, "outgoing_plan_updates"
 
-config :engine, :init_from_storage, System.get_env("INIT_FROM_STORAGE") == "true"
-
 config :logger, level: :info
 config :logger, handle_otp_reports: false
 import_config "#{Mix.env()}.exs"
--- packages/elixir_umbrella/elixir_apps/engine/mix.exs
+++ packages/elixir_umbrella/elixir_apps/engine/mix.exs
@@ -22,7 +22,8 @@ defmodule Engine.MixProject do
       env: [
         amqp_host: "localhost",
         osrm_url: "https://osrm.iteamdev.io",
-        redis_url: "redis://localhost:6379"
+        redis_url: "redis://localhost:6379",
+        init_from_storage: false
       ],
       extra_applications: [:logger, :gproc],
       mod: {Engine.Application, []}

f83ca03e207687a5fd98620cab0a4f882a27df75
Mikael Gråborg <mikael.graborg@iteam.se>
2020-09-28 13:06:49 +0200

    chore: load mocks folder in test

--- packages/engine_umbrella/apps/engine/mix.exs
+++ packages/engine_umbrella/apps/engine/mix.exs
@@ -11,7 +11,8 @@ defmodule Engine.MixProject do
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
@@ -51,4 +52,7 @@ defmodule Engine.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/mocks"]
+  defp elixirc_paths(_), do: ["lib"]
 end

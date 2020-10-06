0b555f290909b593b7f5853699fd2d96d65035ff
Mikael Gråborg <mikael.graborg@iteam.se>
2020-10-06 10:48:41 +0200

    feat: add more booking events to eventstore

--- packages/engine_umbrella/apps/engine/lib/booking.ex
+++ packages/engine_umbrella/apps/engine/lib/booking.ex
@@ -1,6 +1,9 @@
 defmodule Booking do
   use GenServer
   require Logger
+  alias Engine.ES
+  @derive Jason.Encoder
+
   @outgoing_booking_exchange Application.compile_env!(:engine, :outgoing_booking_exchange)
 
   defstruct [
@@ -18,7 +21,7 @@ defmodule Booking do
   def make(pickup, delivery, external_id, metadata, size) do
     id = "pmb-" <> (Base62UUID.generate() |> String.downcase() |> String.slice(0, 8))
 
-    booking = %Booking{
+    %Booking{
       id: id,
       pickup: pickup,
       external_id: external_id,
@@ -28,17 +31,22 @@ defmodule Booking do
       size: size,
       route: Osrm.route(pickup, delivery)
     }
+    |> add_event_to_events_list("new")
+    |> apply_booking_to_state()
+    |> (&ES.add_event(%BookingRegistered{booking: &1})).()
 
+    id
+  end
+
+  def apply_booking_to_state(%Booking{id: id} = booking) do
     GenServer.start_link(
       __MODULE__,
       booking,
       name: via_tuple(id)
     )
 
-    add_event(id, "new")
     Engine.BookingStore.put_booking(id)
-
-    id
+    booking
   end
 
   def get(id), do: GenServer.call(via_tuple(id), :get)
@@ -51,8 +59,18 @@ defmodule Booking do
   end
 
   def add_event(booking_id, status)
-      when status in ["new", "picked_up", "delivered", "delivery_failed"],
-      do: GenServer.call(via_tuple(booking_id), {:add_event, status})
+      when status in ["picked_up", "delivered", "delivery_failed"] do
+    apply_event_to_state(booking_id, status)
+
+    booking_id
+    |> event_to_event_store_struct(status)
+    |> ES.add_event()
+  end
+
+  def apply_event_to_state(booking_id, status),
+    do: GenServer.call(via_tuple(booking_id), {:add_event, status})
+
+  ### Internal
 
   defp via_tuple(id) when is_integer(id), do: via_tuple(Integer.to_string(id))
 
@@ -85,12 +103,28 @@ defmodule Booking do
 
   def handle_call({:add_event, status}, _, state) do
     Logger.info("Received event #{status} for booking: #{state.id} ")
-    new_event = %{timestamp: DateTime.utc_now(), type: String.to_atom(status)}
 
     updated_state =
-      Map.update!(state, :events, fn events -> [new_event | events] end)
+      state
+      |> add_event_to_events_list(status)
       |> MQ.publish(@outgoing_booking_exchange, status)
 
     {:reply, true, updated_state}
   end
+
+  defp add_event_to_events_list(booking, status) do
+    new_event = %{timestamp: DateTime.utc_now(), type: String.to_atom(status)}
+
+    booking
+    |> Map.update!(:events, fn events -> [new_event | events] end)
+  end
+
+  defp event_to_event_store_struct(booking_id, "picked_up"),
+    do: %BookingPickedUp{booking_id: booking_id}
+
+  defp event_to_event_store_struct(booking_id, "delivered"),
+    do: %BookingDelivered{booking_id: booking_id}
+
+  defp event_to_event_store_struct(booking_id, "delivery_failed"),
+    do: %BookingDeliveryFailed{booking_id: booking_id}
 end

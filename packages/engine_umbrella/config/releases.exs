699035f5b5b48a39d9279b0e2dd036643b565c39
Mikael Gråborg <mikael.graborg@iteam.se>
2020-10-12 09:58:51 +0200

    chore: add postgres config to releases

--- packages/engine_umbrella/config/releases.exs
+++ packages/engine_umbrella/config/releases.exs
@@ -1,14 +1,13 @@
 import Config
 config :engine, :amqp_host, System.get_env("AMQP_HOST") || "localhost"
 config :engine, :osrm_url, System.get_env("OSRM_URL") || "https://osrm.iteamdev.io"
-config :engine, :redis_url, System.get_env("REDIS_URL") || "redis://localhost:6379"
 config :engine, :init_from_eventstore, System.get_env("INIT_FROM_EVENTSTORE") == "true"
 
 config :engine,
        :booking_processor_batch_size,
        if(System.get_env("BOOKING_PROCESSOR_BATCH_SIZE") != nil,
-        do: System.get_env("BOOKING_PROCESSOR_BATCH_SIZE") |> String.to_integer(),
-        else: 100
+         do: System.get_env("BOOKING_PROCESSOR_BATCH_SIZE") |> String.to_integer(),
+         else: 100
        )
 
 config :engine,
@@ -17,3 +16,10 @@ config :engine,
          do: System.get_env("BOOKING_PROCESSOR_BATCH_TIMEOUT") |> String.to_integer(),
          else: 100
        )
+
+config :engine, Engine.ES,
+  serializer: EventStore.JsonSerializer,
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  database: System.get_env("POSTGRES_DB"),
+  hostname: System.get_env("POSTGRES_HOST")

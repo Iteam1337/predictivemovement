93b3a186f387761a06f097f3d1a18cf9e9127181
Mikael Gråborg <mikael.graborg@iteam.se>
2020-10-14 16:29:27 +0200

    chore: increase eventEmitter maxlisteners

--- packages/engine-server/lib/index.js
+++ packages/engine-server/lib/index.js
@@ -1,10 +1,12 @@
+const server = require('http').createServer()
+const io = require('socket.io')(server)
+require('events').defaultMaxListeners = 20
+
 const port = 4000
 const bookingsCache = new Map()
 const vehiclesCache = new Map()
 const planCache = new Map()
 
-const server = require('http').createServer()
-const io = require('socket.io')(server)
 server.listen(port)
 
 require('./routes')({ io, bookingsCache, vehiclesCache, planCache })

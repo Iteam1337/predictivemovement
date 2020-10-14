72654da1aeb032903480cf7a9ec3bc60e9d81d41
Radu Achim <radu.achim@iteam.se>
2020-10-14 13:17:27 +0200

    chore: add tests for time contraints validation

--- packages/engine_umbrella/apps/engine/test/vehicle_test.exs
+++ packages/engine_umbrella/apps/engine/test/vehicle_test.exs
@@ -1,3 +1,27 @@
 defmodule VehicleTest do
+  import TestHelper
+  def amqp_url, do: "amqp://" <> Application.fetch_env!(:engine, :amqp_host)
+  @outgoing_plan_exchange Application.compile_env!(:engine, :outgoing_plan_exchange)
   use ExUnit.Case
+
+  @tag :only
+  test "it allows vehicle creation" do
+    MessageGenerator.random_car()
+    |> Vehicle.make()
+
+    vehicle_ids = Engine.VehicleStore.get_vehicles()
+    clear_state()
+    assert vehicle_ids |> length() == 1
+  end
+
+  @tag :only
+  test "does not allow malformed time constraints" do
+    MessageGenerator.random_car(%{ earliest_start: "foo", latest_end: "bar"})
+    |> Vehicle.make()
+
+    vehicle_ids = Engine.VehicleStore.get_vehicles()
+
+    clear_state()
+    assert vehicle_ids |> length() == 0
+  end
 end

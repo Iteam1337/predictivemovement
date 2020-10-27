090ce1ee21470a9475c103e803b889efc9c2d5d8
Simon Lundell <simon.lundell@iteam.se>
2020-10-27 15:25:07 +0100

    style: linting

--- packages/engine-ui/src/components/Plan.tsx
+++ packages/engine-ui/src/components/Plan.tsx
@@ -6,17 +6,17 @@ import PlanRouteDetails from './PlanRouteDetails'
 import Icons from '../assets/Icons'
 import helpers from '../utils/helpers'
 import PlanBookingDetails from './PlanBookingDetails'
-import { Plan as IPlan, Transport, Booking, ExcludedBooking } from '../types'
+import { Plan, Transport, Booking, ExcludedBooking } from '../types'
 import stores from '../utils/state/stores'
 import { FlyToInterpolator } from 'react-map-gl'
 
 const bookingStatusToReadable = (status: string) => {
   switch (status) {
-    case "TIME_CONSTRAINTS_EXPIRED":
-      return "Tidsfönster passerat";
-    case "CONSTRAINTS_FAILURE":
+    case 'TIME_CONSTRAINTS_EXPIRED':
+      return 'Tidsfönster passerat'
+    case 'CONSTRAINTS_FAILURE':
     default:
-      return "Okänd anledning"
+      return 'Okänd anledning'
   }
 }
 
@@ -26,20 +26,18 @@ const Paragraph = styled.p`
   margin-left: 10px;
 `
 
-
 const PlanWrapper = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
 `
 
-interface IPlanProps {
-  plan: IPlan,
+interface PlanProps {
+  plan: Plan
   transports: Transport[]
   dispatchOffers: (params: any) => void
   bookings: Booking[]
 }
-
 
 const BookingToggleList: React.FC<{
   excludedBookings: ExcludedBooking[]
@@ -57,69 +55,64 @@ const BookingToggleList: React.FC<{
   onMouseLeaveHandler,
   isOpen,
   setOpen,
-}) => {
-  return (
-    <Elements.Layout.MarginBottomContainer>
-      <Elements.Layout.FlexRowWrapper onClick={setOpen}>
-        <Elements.Typography.CleanH4>{text}</Elements.Typography.CleanH4>
-        <Icons.Arrow
-          style={{
-            marginLeft: '0.875rem',
-            transform: `rotate(${isOpen ? '180deg' : 0})`,
-          }}
-        />
-      </Elements.Layout.FlexRowWrapper>
+}) => (
+  <Elements.Layout.MarginBottomContainer>
+    <Elements.Layout.FlexRowWrapper onClick={setOpen}>
+      <Elements.Typography.CleanH4>{text}</Elements.Typography.CleanH4>
+      <Icons.Arrow
+        style={{
+          marginLeft: '0.875rem',
+          transform: `rotate(${isOpen ? '180deg' : 0})`,
+        }}
+      />
+    </Elements.Layout.FlexRowWrapper>
 
-      {isOpen && (
-        <Elements.Layout.BookingList>
-          {excludedBookings.map((booking) => (
-              <li key={booking.id}>
-                <Elements.Layout.InlineContainer>
-                  <Elements.Links.RoundedLink
-                    onMouseOver={() => onMouseEnterHandler(booking.id)}
-                    onMouseLeave={() => onMouseLeaveHandler()}
-                    to={`/bookings/${booking.id}`}
-                    onClick={() => onClickHandler(booking.lat, booking.lon)}
-                  >
-                    {helpers.getLastFourChars(booking.id).toUpperCase()}
-                  </Elements.Links.RoundedLink>
-                  <Paragraph>
-                    {bookingStatusToReadable(booking.status)}
-                  </Paragraph>
-                </Elements.Layout.InlineContainer>
-              </li>
-            ))}
-        </Elements.Layout.BookingList>
-      )}
-    </Elements.Layout.MarginBottomContainer>
-  )
-}
+    {isOpen && (
+      <Elements.Layout.BookingList>
+        {excludedBookings.map((booking) => (
+          <li key={booking.id}>
+            <Elements.Layout.InlineContainer>
+              <Elements.Links.RoundedLink
+                onMouseOver={() => onMouseEnterHandler(booking.id)}
+                onMouseLeave={() => onMouseLeaveHandler()}
+                to={`/bookings/${booking.id}`}
+                onClick={() => onClickHandler(booking.lat, booking.lon)}
+              >
+                {helpers.getLastFourChars(booking.id).toUpperCase()}
+              </Elements.Links.RoundedLink>
+              <Paragraph>{bookingStatusToReadable(booking.status)}</Paragraph>
+            </Elements.Layout.InlineContainer>
+          </li>
+        ))}
+      </Elements.Layout.BookingList>
+    )}
+  </Elements.Layout.MarginBottomContainer>
+)
 
-const Plan = ({
-  plan,
-  dispatchOffers,
-  transports,
-  bookings,
-}: IPlanProps) => {
+export default ({ plan, dispatchOffers, transports, bookings }: PlanProps) => {
   const activeRoutes = plan.routes.filter(
     (d) => d.activities && d.activities.length > 0
   )
-  const [expandedSection, setExpandedSection] = React.useState({isOpen: false})
+  const [expandedSection, setExpandedSection] = React.useState({
+    isOpen: false,
+  })
   const { path } = useRouteMatch()
   const setUIState = stores.ui((state) => state.dispatch)
   const setMap = stores.map((state) => state.set)
-  const onClickHandler = (latitude: number, longitude: number) => setMap({
-    latitude,
-    longitude,
-    zoom: 10,
-    transitionDuration: 2000,
-    transitionInterpolator: new FlyToInterpolator(),
-    transitionEasing: (t: number) => t * (2 - t),
-  })
-  const handleExpand = () => setExpandedSection((currentState) => ({
-    ...currentState,
-    isOpen: !currentState.isOpen,
-  }))
+  const onClickHandler = (latitude: number, longitude: number) =>
+    setMap({
+      latitude,
+      longitude,
+      zoom: 10,
+      transitionDuration: 2000,
+      transitionInterpolator: new FlyToInterpolator(),
+      transitionEasing: (t: number) => t * (2 - t),
+    })
+  const handleExpand = () =>
+    setExpandedSection((currentState) => ({
+      ...currentState,
+      isOpen: !currentState.isOpen,
+    }))
   return (
     <Switch>
       <Route exact path={[path, `${path}/routes/:routeId`]}>
@@ -142,20 +135,21 @@ const Plan = ({
                   }
                 />
               ))}
-            {plan.excludedBookings.length > 0 &&
-            <BookingToggleList
-              excludedBookings={plan.excludedBookings}
-              text="Exkluderade bokningar"
-              onClickHandler={onClickHandler}
-              onMouseEnterHandler={(id: string) =>
-                setUIState({ type: 'highlightBooking', payload: id })
-              }
-              onMouseLeaveHandler={() =>
-                setUIState({ type: 'highlightBooking', payload: undefined })
-              }
-              isOpen={expandedSection.isOpen}
-              setOpen={handleExpand}
-            />}
+              {plan.excludedBookings.length > 0 && (
+                <BookingToggleList
+                  excludedBookings={plan.excludedBookings}
+                  text="Exkluderade bokningar"
+                  onClickHandler={onClickHandler}
+                  onMouseEnterHandler={(id: string) =>
+                    setUIState({ type: 'highlightBooking', payload: id })
+                  }
+                  onMouseLeaveHandler={() =>
+                    setUIState({ type: 'highlightBooking', payload: undefined })
+                  }
+                  isOpen={expandedSection.isOpen}
+                  setOpen={handleExpand}
+                />
+              )}
               <Elements.Buttons.SubmitButton
                 alignSelf="center"
                 marginTop="5rem"
@@ -182,5 +176,3 @@ const Plan = ({
     </Switch>
   )
 }
-
-export default Plan

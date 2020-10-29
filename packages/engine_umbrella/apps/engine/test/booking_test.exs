78e20d2f7c21168c4c62a26af2e6e35a2a838edb
Radu Achim <radu.achim@iteam.se>
2020-10-29 14:36:27 +0100

    feat: 🎸 validate vehicle/booking addresses
    they need to include lat/lon and be numbers

--- packages/engine_umbrella/apps/engine/test/booking_test.exs
+++ packages/engine_umbrella/apps/engine/test/booking_test.exs
@@ -67,4 +67,46 @@ defmodule BookingTest do
     assert is_binary(result)
     clear_state()
   end
+
+  test "should validate booking addresses containing lat/lon" do
+    result =
+      MessageGenerator.random_booking()
+      |> Map.put(:pickup, %{city: "", name: "hafdoajgjagia", street: ""})
+      |> Map.put(:delivery, %{city: "", name: "hafdoajgjagia", street: ""})
+      |> Booking.make()
+
+    assert result == [
+             {:error, [:delivery, :lat], :number, "must be a number"},
+             {:error, [:delivery, :lon], :number, "must be a number"},
+             {:error, [:pickup, :lat], :number, "must be a number"},
+             {:error, [:pickup, :lon], :number, "must be a number"}
+           ]
+  end
+
+  test "should validate booking addresses lat/lon in correct format" do
+    result =
+      MessageGenerator.random_booking()
+      |> Map.put(:pickup, %{
+        lat: "2321321",
+        lon: "dkdsakjdsa",
+        city: "",
+        name: "hafdoajgjagia",
+        street: ""
+      })
+      |> Map.put(:delivery, %{
+        lat: "2321321",
+        lon: "dkdsakjdsa",
+        city: "",
+        name: "hafdoajgjagia",
+        street: ""
+      })
+      |> Booking.make()
+
+    assert result == [
+             {:error, [:delivery, :lat], :number, "must be a number"},
+             {:error, [:delivery, :lon], :number, "must be a number"},
+             {:error, [:pickup, :lat], :number, "must be a number"},
+             {:error, [:pickup, :lon], :number, "must be a number"}
+           ]
+  end
 end

06d7c3d852b51bcbff3ba4deaa71d3708847e887
Radu Achim <radu.achim@iteam.se>
2020-11-04 15:36:14 +0100

    chore: remove duplicates from configuration files

--- packages/engine_umbrella/config/runtime.exs
+++ packages/engine_umbrella/config/runtime.exs
@@ -1,15 +1,5 @@
 import Config
 
-config :engine, :plan, Plan
-config :engine, :vehicle, Vehicle
-config :engine, event_stores: [Engine.ES]
-
-config :engine, :outgoing_vehicle_exchange, "outgoing_vehicle_updates"
-config :engine, :incoming_vehicle_exchange, "incoming_vehicle_updates"
-config :engine, :outgoing_booking_exchange, "outgoing_booking_updates"
-config :engine, :incoming_booking_exchange, "incoming_booking_updates"
-config :engine, :outgoing_plan_exchange, "outgoing_plan_updates"
-
 config :engine, :amqp_host, System.get_env("AMQP_HOST") || "localhost"
 config :engine, :osrm_url, System.get_env("OSRM_URL") || "https://osrm.iteamdev.io"
 config :engine, :init_from_eventstore, System.get_env("INIT_FROM_EVENTSTORE") == "true"

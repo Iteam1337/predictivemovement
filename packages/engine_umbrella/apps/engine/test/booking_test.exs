548f0dbd3396d9baae3c055031882c3a079dc07c
Mikael Gråborg <mikael.graborg@iteam.se>
2020-11-17 11:40:34 +0100

    test: update tests for helpers

--- packages/engine_umbrella/apps/engine/test/booking_test.exs
+++ packages/engine_umbrella/apps/engine/test/booking_test.exs
@@ -1,10 +1,11 @@
 defmodule BookingTest do
   import TestHelper
+  alias MessageGenerator.BookingGenerator
   use ExUnit.Case
 
   test "it allows booking creation" do
     result =
-      MessageGenerator.random_booking()
+      BookingGenerator.generate_booking()
       |> Booking.make()
 
     assert is_binary(result)
@@ -13,7 +14,7 @@ defmodule BookingTest do
 
   test "does not allow malformed size (weight)" do
     result =
-      MessageGenerator.random_booking(%{
+      BookingGenerator.generate_booking(%{
         size: %{measurements: [14, 12, 10], weight: 1.2}
       })
       |> Booking.make()
@@ -23,7 +24,7 @@ defmodule BookingTest do
 
   test "does not allow malformed size (measurements)" do
     result =
-      MessageGenerator.random_booking(%{
+      BookingGenerator.generate_booking(%{
         size: %{measurements: "12", weight: 1}
       })
       |> Booking.make()
@@ -36,7 +37,7 @@ defmodule BookingTest do
 
   test "does not allow malformed measurements elements" do
     result =
-      MessageGenerator.random_booking(%{
+      BookingGenerator.generate_booking(%{
         size: %{measurements: ["12"], weight: 1}
       })
       |> Booking.make()
@@ -49,7 +50,7 @@ defmodule BookingTest do
 
   test "does not allow incomplete measurements" do
     result =
-      MessageGenerator.random_booking(%{
+      BookingGenerator.generate_booking(%{
         size: %{measurements: [12], weight: 1}
       })
       |> Booking.make()
@@ -59,7 +60,7 @@ defmodule BookingTest do
 
   test "allows correct measurements" do
     result =
-      MessageGenerator.random_booking(%{
+      BookingGenerator.generate_booking(%{
         size: %{measurements: [12, 14, 15], weight: 1}
       })
       |> Booking.make()
@@ -70,7 +71,7 @@ defmodule BookingTest do
 
   test "should validate booking addresses containing lat/lon" do
     result =
-      MessageGenerator.random_booking()
+      BookingGenerator.generate_booking()
       |> Map.put(:pickup, %{city: "", name: "hafdoajgjagia", street: ""})
       |> Map.put(:delivery, %{city: "", name: "hafdoajgjagia", street: ""})
       |> Booking.make()
@@ -85,7 +86,7 @@ defmodule BookingTest do
 
   test "should validate booking addresses lat/lon in correct format" do
     result =
-      MessageGenerator.random_booking()
+      BookingGenerator.generate_booking()
       |> Map.put(:pickup, %{
         lat: "2321321",
         lon: "dkdsakjdsa",
@@ -111,14 +112,14 @@ defmodule BookingTest do
   end
 
   test "requires that delivery is present" do
-    MessageGenerator.random_booking()
+    BookingGenerator.generate_booking()
     |> Map.delete(:delivery)
     |> Booking.make()
     |> catch_error()
   end
 
   test "requires that pickup is present" do
-    MessageGenerator.random_booking()
+    BookingGenerator.generate_booking()
     |> Map.delete(:pickup)
     |> Booking.make()
     |> catch_error()
@@ -126,7 +127,7 @@ defmodule BookingTest do
 
   test "should allow booking to be updated" do
     id =
-      MessageGenerator.random_booking()
+      BookingGenerator.generate_booking()
       |> Booking.make()
 
     updated_booking = %{
@@ -149,7 +150,7 @@ defmodule BookingTest do
 
   test "allows for only updating delivery" do
     id =
-      MessageGenerator.random_booking()
+      BookingGenerator.generate_booking()
       |> Booking.make()
 
     new_delivery = %{lat: 13.37, lon: 13.37}
@@ -165,7 +166,7 @@ defmodule BookingTest do
 
   test "allows for only updating pickup" do
     id =
-      MessageGenerator.random_booking()
+      BookingGenerator.generate_booking()
       |> Booking.make()
 
     new_pickup = %{lat: 13.37, lon: 13.37}
@@ -181,7 +182,7 @@ defmodule BookingTest do
 
   test "allows for only updating size" do
     id =
-      MessageGenerator.random_booking()
+      BookingGenerator.generate_booking()
       |> Booking.make()
 
     new_size = %{measurements: [222, 2, 2], weight: 100}

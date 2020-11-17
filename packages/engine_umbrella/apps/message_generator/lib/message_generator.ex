97b7dbde2f1bae174e92833ecb08ab6a899f965b
Mikael Gråborg <mikael.graborg@iteam.se>
2020-11-17 12:04:35 +0100

    chore: remove random from generator names

--- packages/engine_umbrella/apps/message_generator/lib/message_generator.ex
+++ packages/engine_umbrella/apps/message_generator/lib/message_generator.ex
@@ -6,28 +6,28 @@ defmodule MessageGenerator do
   @transports_exchange "incoming_vehicle_updates"
   @bookings_exchange "incoming_booking_updates"
 
-  def add_random_transport(properties \\ %{})
+  def add_transport(properties \\ %{})
 
-  def add_random_transport(properties) when is_map(properties) do
+  def add_transport(properties) when is_map(properties) do
     TransportGenerator.generate_transport_props(properties)
     |> RMQ.publish(@transports_exchange, "registered")
   end
 
-  def add_random_transport(city) when city in [:stockholm, :gothenburg] do
+  def add_transport(city) when city in [:stockholm, :gothenburg] do
     %{}
     |> TransportGenerator.put_new_transport_addresses_from_city(city)
     |> TransportGenerator.generate_transport_props()
     |> RMQ.publish(@transports_exchange, "registered")
   end
 
-  def add_random_booking(properties \\ %{})
+  def add_booking(properties \\ %{})
 
-  def add_random_booking(properties) when is_map(properties) do
+  def add_booking(properties) when is_map(properties) do
     BookingGenerator.generate_booking_props(properties)
     |> RMQ.publish(@bookings_exchange, "registered")
   end
 
-  def add_random_booking(city) when city in [:stockholm, :gothenburg] do
+  def add_booking(city) when city in [:stockholm, :gothenburg] do
     %{}
     |> BookingGenerator.put_new_booking_addresses_from_city(city)
     |> BookingGenerator.generate_booking_props()

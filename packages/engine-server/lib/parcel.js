e3d84e5397a2c310101fd386970b050705a99fd8
Radu Achim <radu.achim@iteam.se>
2020-11-17 09:37:25 +0100

    chore: use postnord api v5

--- packages/engine-server/lib/parcel.js
+++ packages/engine-server/lib/parcel.js
@@ -3,7 +3,7 @@ const postnordKey = process.env.POSTNORD_KEY
 
 const search = (id) => {
   return fetch(
-    `https://api2.postnord.com/rest/shipment/v1/trackandtrace/findByIdentifier.json?apikey=${postnordKey}&id=${id}&locale=en`
+    `https://api2.postnord.com/rest/shipment/v5/trackandtrace/findByIdentifier.json?apikey=${postnordKey}&id=${id}&locale=en`
   )
 }
 

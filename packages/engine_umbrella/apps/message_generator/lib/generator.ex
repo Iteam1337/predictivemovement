89ba550eb50ab92bc9d13d684f0cb593080fd953
Mikael <mikael.graborg@iteam.se>
2020-11-18 12:10:49 +0100

    chore: clean message generator (#277)
    * chore: use adapter for message generator
    
    * chore: clean up add car in message generator
    
    * chore(msg-gen): change name from car to transport
    
    * chore: remove msg-gen genserver
    
    * chore: remove ID from transport
    
    * chore: clean up add_transport more
    
    * chore: rename vehicle to transport
    
    * chore(msg-generator): clean up add_booking
    
    * refactor(msg-gen): move code into generator helper
    
    * refactor(msg-gen): make function name more verbose
    
    * refactor(msg-gen): create more helpers
    
    * test: update tests for helpers
    
    * fix: put_new_booking_addresses_from_city signature
    
    * tests: fix them
    
    * refactor: rename generator funcs
    
    * chore: remove random from generator names
    
    * chore: rename message generator to generator

---
+++ packages/engine_umbrella/apps/message_generator/lib/generator.ex
@@ -0,0 +1,36 @@
+defmodule Generator do
+  alias MessageGenerator.Adapters.RMQ
+  alias MessageGenerator.TransportGenerator
+  alias MessageGenerator.BookingGenerator
+
+  @transports_exchange "incoming_vehicle_updates"
+  @bookings_exchange "incoming_booking_updates"
+
+  def add_transport(properties \\ %{})
+
+  def add_transport(properties) when is_map(properties) do
+    TransportGenerator.generate_transport_props(properties)
+    |> RMQ.publish(@transports_exchange, "registered")
+  end
+
+  def add_transport(city) when city in [:stockholm, :gothenburg] do
+    %{}
+    |> TransportGenerator.put_new_transport_addresses_from_city(city)
+    |> TransportGenerator.generate_transport_props()
+    |> RMQ.publish(@transports_exchange, "registered")
+  end
+
+  def add_booking(properties \\ %{})
+
+  def add_booking(properties) when is_map(properties) do
+    BookingGenerator.generate_booking_props(properties)
+    |> RMQ.publish(@bookings_exchange, "registered")
+  end
+
+  def add_booking(city) when city in [:stockholm, :gothenburg] do
+    %{}
+    |> BookingGenerator.put_new_booking_addresses_from_city(city)
+    |> BookingGenerator.generate_booking_props()
+    |> RMQ.publish(@bookings_exchange, "registered")
+  end
+end

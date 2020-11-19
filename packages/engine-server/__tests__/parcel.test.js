1a23eacf3ff52f9a628e341606d0a3098489320a
Mikael Gråborg <mikael.graborg@iteam.se>
2020-11-19 15:39:02 +0100

    test: add getMeasurements tests

--- packages/engine-server/__tests__/parcel.test.js
+++ packages/engine-server/__tests__/parcel.test.js
@@ -1,67 +1,66 @@
-const { getWeight } = require('../lib/parcel')
-
-const WEIGHT_PAYLOAD = {
-  TrackingInformationResponse: {
-    shipments: [
-      {
-        totalWeight: {
-          value: '300',
-          unit: 'kg',
-        },
-        assessedWeight: {
-          value: '400',
-          unit: 'kg',
-        },
-        items: [
-          {
-            itemId: '1337',
-            statedMeasurement: {
-              weight: {
-                value: '1',
-                unit: 'kg',
-              },
-            },
-            assessedMeasurement: {
-              weight: {
-                value: '7',
-                unit: 'kg',
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-}
+const { getWeight, getMeasurements } = require('../lib/parcel')
 
 describe('extract weight', () => {
+  const weightPayload = {
+    TrackingInformationResponse: {
+      shipments: [
+        {
+          totalWeight: {
+            value: '300',
+            unit: 'kg',
+          },
+          assessedWeight: {
+            value: '400',
+            unit: 'kg',
+          },
+          items: [
+            {
+              itemId: '1337',
+              statedMeasurement: {
+                weight: {
+                  value: '1',
+                  unit: 'kg',
+                },
+              },
+              assessedMeasurement: {
+                weight: {
+                  value: '7',
+                  unit: 'kg',
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  }
   it('should return statedMeasurement firstly', () => {
-    const weight = getWeight(WEIGHT_PAYLOAD)
+    const weight = getWeight(weightPayload)
     expect(weight).toBe(1)
   })
 
   it('should return totalWeight secondly', () => {
-    delete WEIGHT_PAYLOAD.TrackingInformationResponse.shipments[0].items[0]
+    delete weightPayload.TrackingInformationResponse.shipments[0].items[0]
       .statedMeasurement
-    const weight = getWeight(WEIGHT_PAYLOAD)
+    const weight = getWeight(weightPayload)
     expect(weight).toBe(300)
   })
 
   it('should return assessedMeasurement 3rd', () => {
-    delete WEIGHT_PAYLOAD.TrackingInformationResponse.shipments[0].items[0]
+    delete weightPayload.TrackingInformationResponse.shipments[0].items[0]
       .statedMeasurement
-    delete WEIGHT_PAYLOAD.TrackingInformationResponse.shipments[0].totalWeight
-    const weight = getWeight(WEIGHT_PAYLOAD)
+    delete weightPayload.TrackingInformationResponse.shipments[0].totalWeight
+    const weight = getWeight(weightPayload)
     expect(weight).toBe(7)
   })
 
   it('should return assessedWeight 4th', () => {
-    delete WEIGHT_PAYLOAD.TrackingInformationResponse.shipments[0].items[0]
+    delete weightPayload.TrackingInformationResponse.shipments[0].items[0]
       .statedMeasurement
-    delete WEIGHT_PAYLOAD.TrackingInformationResponse.shipments[0].totalWeight
-    delete WEIGHT_PAYLOAD.TrackingInformationResponse.shipments[0].items[0]
+    delete weightPayload.TrackingInformationResponse.shipments[0].totalWeight
+    delete weightPayload.TrackingInformationResponse.shipments[0].items[0]
       .assessedMeasurement
-    const weight = getWeight(WEIGHT_PAYLOAD)
+    const weight = getWeight(weightPayload)
     expect(weight).toBe(400)
   })
 
@@ -139,40 +138,32 @@ describe('extract weight', () => {
 })
 
 describe('extract measurements', () => {
-  const data = {
+  const measurementsPayload = {
     TrackingInformationResponse: {
       shipments: [
         {
-          totalWeight: {
-            weight: {
-              value: '300',
-              unit: 'kg',
-            },
+          totalVolume: {
+            value: '2',
+            unit: 'm3',
           },
-          assessedWeight: {
-            weight: {
-              value: '400',
-              unit: 'kg',
-            },
+          assessedVolume: {
+            value: '3',
+            unit: 'm3',
           },
           items: [
             {
               itemId: '1337',
               statedMeasurement: {
-                weight: {
-                  value: '1',
-                  unit: 'kg',
-                },
                 length: {
-                  value: '0.31',
+                  value: '3',
                   unit: 'm',
                 },
                 height: {
-                  value: '0.17',
+                  value: '1',
                   unit: 'm',
                 },
                 width: {
-                  value: '0.24',
+                  value: '2',
                   unit: 'm',
                 },
                 volume: {
@@ -181,9 +172,21 @@ describe('extract measurements', () => {
                 },
               },
               assessedMeasurement: {
-                weight: {
-                  value: '7',
-                  unit: 'kg',
+                length: {
+                  value: '4',
+                  unit: 'm',
+                },
+                height: {
+                  value: '5',
+                  unit: 'm',
+                },
+                width: {
+                  value: '6',
+                  unit: 'm',
+                },
+                volume: {
+                  value: '0.012',
+                  unit: 'm3',
                 },
               },
             },
@@ -192,4 +195,142 @@ describe('extract measurements', () => {
       ],
     },
   }
+
+  it('should return statedMeasurement firstly', () => {
+    const measurements = getMeasurements(measurementsPayload)
+    expect(measurements).toBe(2, 1, 3)
+  })
+  it('should return assessedMeasurement secondly', () => {
+    const measurements = getMeasurements(measurementsPayload)
+    delete measurementsPayload.TrackingInformationResponse.shipments[0].items[0]
+      .statedMeasurement
+    expect(measurements).toBe(6, 5, 4)
+  })
+  it('should round the values', () => {
+    const data = {
+      TrackingInformationResponse: {
+        shipments: [
+          {
+            items: [
+              {
+                itemId: '1337',
+                statedMeasurement: {
+                  length: {
+                    value: '0.4',
+                    unit: 'cm',
+                  },
+                  height: {
+                    value: '0.5',
+                    unit: 'cm',
+                  },
+                  width: {
+                    value: '0.6',
+                    unit: 'cm',
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    }
+    const measurements = getWeight(data)
+    expect(measurements).toBe([1, 2, 0])
+  })
+
+  it('should convert m to cm', () => {
+    const data = {
+      TrackingInformationResponse: {
+        shipments: [
+          {
+            items: [
+              {
+                itemId: '1337',
+                statedMeasurement: {
+                  length: {
+                    value: '4',
+                    unit: 'm',
+                  },
+                  height: {
+                    value: '5',
+                    unit: 'm',
+                  },
+                  width: {
+                    value: '6',
+                    unit: 'm',
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    }
+    const measurements = getWeight(data)
+    expect(measurements).toBe([600, 500, 400])
+  })
+
+  it('should convert dm to cm', () => {
+    const data = {
+      TrackingInformationResponse: {
+        shipments: [
+          {
+            items: [
+              {
+                itemId: '1337',
+                statedMeasurement: {
+                  length: {
+                    value: '4',
+                    unit: 'dm',
+                  },
+                  height: {
+                    value: '5',
+                    unit: 'dm',
+                  },
+                  width: {
+                    value: '6',
+                    unit: 'dm',
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    }
+    const measurements = getWeight(data)
+    expect(measurements).toBe([60, 50, 40])
+  })
+
+  it('should round mm to cm-int', () => {
+    const data = {
+      TrackingInformationResponse: {
+        shipments: [
+          {
+            items: [
+              {
+                itemId: '1337',
+                statedMeasurement: {
+                  length: {
+                    value: '435',
+                    unit: 'mm',
+                  },
+                  height: {
+                    value: '572',
+                    unit: 'mm',
+                  },
+                  width: {
+                    value: '600',
+                    unit: 'mm',
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    }
+    const measurements = getWeight(data)
+    expect(measurements).toBe([60, 60, 40])
+  })
 })

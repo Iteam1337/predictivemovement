907ecdb56571ab0fad1cb5a0bb8b14e1bd71334c
Mikael Gråborg <mikael.graborg@iteam.se>
2020-12-01 17:04:30 +0100

    tests: make them work with trimmed down rmq msgs

--- packages/engine_umbrella/apps/engine/test/booking_updates_processor_test.exs
+++ packages/engine_umbrella/apps/engine/test/booking_updates_processor_test.exs
@@ -46,7 +46,8 @@ defmodule BookingUpdatesProcessorTest do
     Booking.assign(booking_id, Vehicle.get(vehicle_id))
     send_status_msg(booking_id, vehicle_id, "picked_up")
     update = wait_for_message(channel)
-    assert Map.get(update, :metadata) == "{\"senderId\":\"telegramIdString\"}"
+    assert Map.get(update, :id) == booking_id
+    assert Map.get(update, :events) |> List.first() |> Map.get(:type) == "picked_up"
 
     assert :picked_up ==
              Booking.get(booking_id)
@@ -72,7 +73,8 @@ defmodule BookingUpdatesProcessorTest do
     Booking.assign(booking_id, Vehicle.get(vehicle_id))
     send_status_msg(booking_id, vehicle_id, "delivered")
     update = wait_for_message(channel)
-    assert Map.get(update, :metadata) == "{\"senderId\":\"telegramIdString\"}"
+    assert Map.get(update, :id) == booking_id
+    assert Map.get(update, :events) |> List.first() |> Map.get(:type) == "delivered"
 
     assert :delivered ==
              Booking.get(booking_id)

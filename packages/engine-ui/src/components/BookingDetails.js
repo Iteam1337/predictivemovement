af1f840097fbaac6a668c6c4fd5aa28a4591dfce
Mikael Gråborg <mikael.graborg@iteam.se>
2020-12-03 16:15:18 +0100

    refactor: put moment format code in func

--- packages/engine-ui/src/components/BookingDetails.js
+++ packages/engine-ui/src/components/BookingDetails.js
@@ -65,6 +65,10 @@ const Timeline = styled.div`
   }
 `
 
+const formatDateLong = (date) => moment(date).format('YYYY-MM-DD, HH:mm')
+
+const formatDateShort = (date) => moment(date).format('HH:mm')
+
 const BookingDetails = ({ bookings, deleteBooking, onUnmount }) => {
   const { bookingId } = useParams()
   const history = useHistory()
@@ -192,18 +196,18 @@ const BookingDetails = ({ bookings, deleteBooking, onUnmount }) => {
             </Elements.Typography.StrongParagraph>
             <CapitalizeParagraph>{address.pickup}</CapitalizeParagraph>
             {pickup.time_windows &&
-              pickup.time_windows.map((timeWindow) => (
-                <Elements.Typography.SmallInfoBold key={timeWindow.earliest}>
-                  {moment(timeWindow.earliest).isSame(timeWindow.latest, 'day')
-                    ? `${moment(timeWindow.earliest).format(
-                        'YYYY-MM-DD, HH:mm'
-                      )} - ${moment(timeWindow.latest).format('HH:mm')}`
-                    : `${moment(timeWindow.earliest).format(
-                        'YYYY-MM-DD, HH:mm'
-                      )} -
-              ${moment(timeWindow.latest).format('YYYY-MM-DD, HH:mm')}`}
-                </Elements.Typography.SmallInfoBold>
-              ))}
+              pickup.time_windows.map(({ earliest, latest }) => {
+                const isSameDay = moment(earliest).isSame(latest, 'day')
+                return (
+                  <Elements.Typography.SmallInfoBold key={earliest}>
+                    {`${formatDateLong(earliest)} - ${
+                      isSameDay
+                        ? formatDateShort(latest)
+                        : formatDateLong(latest)
+                    }`}
+                  </Elements.Typography.SmallInfoBold>
+                )
+              })}
           </Elements.Layout.MarginBottomContainer>
           {sender.name && (
             <Elements.Layout.FlexRowBaselineContainer>
@@ -228,20 +232,19 @@ const BookingDetails = ({ bookings, deleteBooking, onUnmount }) => {
               Avlämning
             </Elements.Typography.StrongParagraph>
             <CapitalizeParagraph>{address.delivery}</CapitalizeParagraph>
-
             {delivery.time_windows &&
-              delivery.time_windows.map((timeWindow) => (
-                <Elements.Typography.SmallInfoBold key={timeWindow.earliest}>
-                  {moment(timeWindow.earliest).isSame(timeWindow.latest, 'day')
-                    ? `${moment(timeWindow.earliest).format(
-                        'YYYY-MM-DD, HH:mm'
-                      )} - ${moment(timeWindow.latest).format('HH:mm')}`
-                    : `${moment(timeWindow.earliest).format(
-                        'YYYY-MM-DD, HH:mm'
-                      )} -
-              ${moment(timeWindow.latest).format('YYYY-MM-DD, HH:mm')}`}
-                </Elements.Typography.SmallInfoBold>
-              ))}
+              delivery.time_windows.map(({ earliest, latest }) => {
+                const isSameDay = moment(earliest).isSame(latest, 'day')
+                return (
+                  <Elements.Typography.SmallInfoBold key={earliest}>
+                    {`${formatDateLong(earliest)} - ${
+                      isSameDay
+                        ? formatDateShort(latest)
+                        : formatDateLong(latest)
+                    }`}
+                  </Elements.Typography.SmallInfoBold>
+                )
+              })}
           </Elements.Layout.MarginBottomContainer>
           {sender.name && (
             <Elements.Layout.FlexRowBaselineContainer>

a063f9e98fcdfd48cf31cd514d7d5bae16b6a2b6
momentiris <momentiris@gmail.com>
2020-12-07 10:05:43 +0100

    chore: clean up html a bit

--- packages/engine-ui/src/components/RouteActivities.tsx
+++ packages/engine-ui/src/components/RouteActivities.tsx
@@ -6,12 +6,14 @@ import * as helpers from '../utils/helpers'
 import * as Elements from '../shared-elements'
 import { useRouteMatch } from 'react-router-dom'
 import * as stores from '../utils/state/stores'
+import triangleIcon from '../assets/triangle.svg'
 
 const ActivityGroup = styled.div`
   margin-left: auto;
   display: flex;
   flex-direction: column;
 `
+
 const ActivityInfo = styled.div`
   display: flex;
   flex-direction: column;
@@ -34,55 +36,53 @@ const Wrapper = styled.div`
   overflow: auto;
 `
 
-const PickUpInfo = styled.div`
-  border-left: 3px solid #19aa6e;
-  flex: 4;
-  height: 130px;
-  padding: 1rem;
+const TimeContainer = styled.div`
+  margin-right: 0.5rem;
+  flex-basis: 15%;
 `
 
-const Circle = styled.div<{ active: boolean }>`
-  background-color: ${(props) => (props.active ? '#d4693f' : '#fff')};
-  border: 3px solid #19aa6e;
-  height: 15px;
-  width: 15px;
-  border-radius: 50%;
-  top: 43%;
-  margin-left: -3.15rem;
+const ActivityListItemContainer = styled.div`
+  padding: 1rem;
+  border-left: 3px solid #19aa6e;
+  flex: 1;
   position: relative;
-  z-index: 2;
+  display: flex;
+  :after {
+    content: '';
+    position: absolute;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background: white;
+    left: -12px;
+    border: 3px solid #19aa6e;
+    top: calc(50% - 8px);
+  }
 `
 
 const SpeechBubble = styled.div`
   position: relative;
-  border: 2px solid #19aa6e;
+  border: 1px solid rgba(0, 0, 0, 0.3);
   text-align: center;
-  padding: 7px;
+  padding: 9px;
   border-radius: 5px;
-  margin-left: 1rem;
-  margin-right: -1rem;
+  flex: 1;
+  text-align: left;
   height: 100%;
+  margin-left: 0.5rem;
 
-  :after {
-    content: '';
-    position: absolute;
-    display: block;
-    z-index: 1;
-    border-style: solid;
-    border-color: transparent #19aa6e;
-    border-width: 10px 10px 10px 0;
-    top: 50%;
-    left: -10px;
-    margin-top: -10px;
+  :hover {
+    border: 1px solid rgba(0, 0, 0, 0.6);
   }
 `
 
-const TimeWrapper = styled.div`
+const TimelineItem = styled.div`
   display: flex;
   width: 100%;
   align-items: center;
   height: 100%;
 `
+
 const EndOfTransportWrapper = styled.div`
   display: flex;
   justify-content: space-between;
@@ -98,34 +98,34 @@ interface Props {
   route: Route
 }
 
-const groupByLocation = (activities: Activity[]) => {
-  const { data } = activities.reduce<{
-    type: string
-    address: string
-    data: Activity[][]
-  }>(
-    (prev, curr) => {
-      const currAddress = JSON.stringify(curr.address)
-      if (prev.type === curr.type && prev.address === currAddress) {
-        const [last] = prev.data.slice(0).reverse()
+// const groupByLocation = (activities: Activity[]) => {
+//   const { data } = activities.reduce<{
+//     type: string
+//     address: string
+//     data: Activity[][]
+//   }>(
+//     (prev, curr) => {
+//       const currAddress = JSON.stringify(curr.address)
+//       if (prev.type === curr.type && prev.address === currAddress) {
+//         const [last] = prev.data.slice(0).reverse()
 
-        return {
-          ...prev,
-          data: [...prev.data.slice(0, -1), [...last, curr]],
-        }
-      }
+//         return {
+//           ...prev,
+//           data: [...prev.data.slice(0, -1), [...last, curr]],
+//         }
+//       }
 
-      return {
-        type: curr.type,
-        address: currAddress,
-        data: [...prev.data, [curr]],
-      }
-    },
-    { type: '', address: '', data: [] }
-  )
+//       return {
+//         type: curr.type,
+//         address: currAddress,
+//         data: [...prev.data, [curr]],
+//       }
+//     },
+//     { type: '', address: '', data: [] }
+//   )
 
-  return data
-}
+//   return data
+// }
 
 const RouteActivities = ({ route }: Props) => {
   const setUIState = stores.ui((state) => state.dispatch)
@@ -167,82 +167,69 @@ const RouteActivities = ({ route }: Props) => {
       <Elements.Typography.BoldParagraph>
         Start
       </Elements.Typography.BoldParagraph>
-      {groupByLocation(activities).map((activityGroup, index) => {
+      {activities.map((activity, index) => {
         return (
           <ActivityInfo key={index}>
-            <TimeWrapper>
-              <div style={{ flex: '1' }}>
+            <TimelineItem>
+              <TimeContainer>
                 <SmallParagraph>
-                  {getDuration(activityGroup[0].duration)} min
+                  {getDuration(activity.duration)} min{' '}
                 </SmallParagraph>
                 <SmallParagraph>
-                  ({getDistance(activityGroup[0].distance)} km)
+                  ({getDistance(activity.distance)} km)
                 </SmallParagraph>
-              </div>
-              <PickUpInfo style={{ flex: '4' }}>
+              </TimeContainer>
+
+              <ActivityListItemContainer>
                 <SpeechBubble>
-                  <Circle active={false} />
                   <div
                     style={{
-                      width: '100%',
                       display: 'flex',
-                      justifyContent: 'space-between',
-                      alignItems: 'center',
-                      marginTop: '-15px',
+                      alignItems: 'baseline',
+                      marginBottom: '.5rem',
                     }}
                   >
-                    <p
-                      style={{
-                        fontSize: '0.80rem',
-                        fontWeight: 'bold',
-                      }}
-                    >
-                      {getLabelForActivities(activityGroup[0].type)}
-                    </p>
+                    <Elements.Typography.InfoMdStrong>
+                      {getLabelForActivities(activity.type)}
+                    </Elements.Typography.InfoMdStrong>
                     <ActivityGroup>
-                      {activityGroup.map((activity, i) => (
-                        <Elements.Links.RoundedLink
-                          key={i}
-                          onMouseOver={() =>
-                            setUIState({
-                              type: 'highlightBooking',
-                              payload: activity.id,
-                            })
-                          }
-                          onMouseLeave={() =>
-                            setUIState({
-                              type: 'highlightBooking',
-                              payload: undefined,
-                            })
-                          }
-                          to={() => redirectTo(activity.id)}
-                          onClick={() =>
-                            setMap({
-                              latitude: activity.address.lat,
-                              longitude: activity.address.lon,
-                              zoom: 10,
-                              transitionDuration: 2000,
-                              transitionInterpolator: new FlyToInterpolator(),
-                              transitionEasing: (t: number) => t * (2 - t),
-                            })
-                          }
-                        >
-                          {helpers.getLastFourChars(activity.id).toUpperCase()}
-                        </Elements.Links.RoundedLink>
-                      ))}
+                      <Elements.Links.RoundedLink
+                        onMouseOver={() =>
+                          setUIState({
+                            type: 'highlightBooking',
+                            payload: activity.id,
+                          })
+                        }
+                        onMouseLeave={() =>
+                          setUIState({
+                            type: 'highlightBooking',
+                            payload: undefined,
+                          })
+                        }
+                        to={() => redirectTo(activity.id)}
+                        onClick={() =>
+                          setMap({
+                            latitude: activity.address.lat,
+                            longitude: activity.address.lon,
+                            zoom: 10,
+                            transitionDuration: 2000,
+                            transitionInterpolator: new FlyToInterpolator(),
+                            transitionEasing: (t: number) => t * (2 - t),
+                          })
+                        }
+                      >
+                        {helpers.getLastFourChars(activity.id).toUpperCase()}
+                      </Elements.Links.RoundedLink>
                     </ActivityGroup>
                   </div>
+
                   <div style={{ textAlign: 'left' }}>
-                    <SmallParagraph>
-                      {activityGroup[0].address.street}
-                    </SmallParagraph>
-                    <SmallParagraph>
-                      {activityGroup[0].address.city}
-                    </SmallParagraph>
+                    <SmallParagraph>{activity.address.street}</SmallParagraph>
+                    <SmallParagraph>{activity.address.city}</SmallParagraph>
                   </div>
                 </SpeechBubble>
-              </PickUpInfo>
-            </TimeWrapper>
+              </ActivityListItemContainer>
+            </TimelineItem>
           </ActivityInfo>
         )
       })}

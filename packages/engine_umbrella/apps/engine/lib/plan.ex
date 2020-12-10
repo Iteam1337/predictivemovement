aeaccf1eef34ea8ddccf5280377a55075491bf51
Mikael Gråborg <mikael.graborg@iteam.se>
2020-12-10 10:22:57 +0100

    fix: publish current_route in plan

--- packages/engine_umbrella/apps/engine/lib/plan.ex
+++ packages/engine_umbrella/apps/engine/lib/plan.ex
@@ -113,6 +113,7 @@ defmodule Plan do
         )
       end)
       |> Enum.map(&add_distance_durations/1)
+      |> Enum.map(&Map.from_struct/1)
 
     PlanStore.put_plan(%{
       vehicles: vehicles,

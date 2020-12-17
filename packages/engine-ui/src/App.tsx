e0c31b1e1f10b43006a87b8468157c1314003b66
momentiris <momentiris@gmail.com>
2020-12-17 16:48:46 +0100

    sign parcel view

--- packages/engine-ui/src/App.tsx
+++ packages/engine-ui/src/App.tsx
@@ -1,55 +1,14 @@
 import React from 'react'
 import { useSocket } from 'use-socketio'
-import Sidebar from './components/Sidebar'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { reducer, initState } from './utils/reducer'
-import { Route } from 'react-router-dom'
-import Map from './components/Map'
-import Logotype from './components/Logotype'
-import * as hooks from './utils/hooks'
-import Notifications from './components/Notifications'
-import * as notificationTypes from './notificationTypes'
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './Home'
+import SignParcel from './SignParcel'
 
 const App = () => {
-  const { socket } = useSocket()
   const [state, dispatch] = React.useReducer(reducer, initState)
-  const { data: mapData } = hooks.useFilteredStateFromQueryParams(state)
-  const [notifications, updateNotifications] = React.useState<
-    notificationTypes.Notification[]
-  >([])
-  const isMobile = window.innerWidth <= 645
-
-  const createTransport = (params: any) => {
-    socket.emit('create-transport', params)
-  }
-
-  const createBooking = (params: any) => {
-    socket.emit('new-booking', params)
-  }
-
-  const dispatchOffers = () => {
-    socket.emit('dispatch-offers')
-  }
-
-  const deleteBooking = (id: string) => {
-    socket.emit('delete-booking', id)
-  }
-
-  const deleteTransport = (id: string) => {
-    socket.emit('delete-transport', id)
-  }
-
-  const moveBooking = (bookingId: string, transportId: string) => {
-    socket.emit('move-booking', { bookingId, transportId })
-  }
-
-  const updateBooking = (booking: any) => {
-    socket.emit('update-booking', booking)
-  }
-
-  useSocket('notification', (data: notificationTypes.Notification) => {
-    updateNotifications((notifications) => notifications.concat(data))
-  })
 
   useSocket('bookings', (bookings) => {
     dispatch({
@@ -96,30 +55,20 @@ const App = () => {
     })
   })
 
+  const handleParcelSignatureConfirm = (signature: string) => {
+    console.log(signature)
+  }
+
   return (
-    <>
-      {!isMobile && <Logotype />}
-      <Notifications
-        notifications={notifications}
-        updateNotifications={updateNotifications}
-      />
-      <Sidebar
-        {...state}
-        isMobile={isMobile}
-        createBooking={createBooking}
-        dispatchOffers={dispatchOffers}
-        createTransport={createTransport}
-        deleteBooking={deleteBooking}
-        deleteTransport={deleteTransport}
-        moveBooking={moveBooking}
-        updateBooking={updateBooking}
-      />
-      {!isMobile && (
-        <Route path="/">
-          <Map data={mapData} state={state} />
-        </Route>
-      )}
-    </>
+    <Switch>
+      <Route path="/sign/:id">
+        <SignParcel onSubmit={handleParcelSignatureConfirm} />
+      </Route>
+
+      <Route path="/">
+        <Home {...state} />
+      </Route>
+    </Switch>
   )
 }
 
---
+++ packages/engine-ui/src/SignParcel.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import SignatureCanvas from 'react-signature-canvas'
+import styled from 'styled-components'
+import * as Elements from './shared-elements/'
+
+const Container = styled.div`
+  padding: 1.875rem;
+  display: flex;
+  flex-flow: column nowrap;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  gap: 1.875rem;
+  max-width: 500px;
+`
+
+const CanvasContainer = styled.div`
+  background: white;
+  width: 100%;
+  height: 200px;
+  border: 2px dashed grey;
+`
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 0.875rem;
+`
+
+const Component: React.FC<{
+  onSubmit: (base64Signature: string) => void
+}> = ({ onSubmit }) => {
+  const nodeRef = React.useRef<SignatureCanvas>(null)
+
+  const clear = () => nodeRef?.current?.clear()
+
+  const confirm = () => {
+    const signatureAsBase64 = nodeRef?.current?.getTrimmedCanvas().toDataURL()
+    if (!nodeRef.current?.isEmpty() && signatureAsBase64)
+      onSubmit(signatureAsBase64)
+  }
+
+  return (
+    <Container>
+      <div>
+        <Elements.Typography.BoldParagraph>
+          Rita din signatur i rutan.
+        </Elements.Typography.BoldParagraph>
+        <CanvasContainer>
+          <SignatureCanvas
+            ref={nodeRef}
+            penColor="black"
+            canvasProps={{
+              className: 'signature_canvas',
+            }}
+          />
+        </CanvasContainer>
+      </div>
+      <ButtonContainer>
+        <Elements.Buttons.CancelButton
+          onClick={clear}
+          color="#666666"
+          width="150px"
+        >
+          Rensa
+        </Elements.Buttons.CancelButton>
+        <Elements.Buttons.SubmitButton
+          onClick={confirm}
+          color="#666666"
+          width="150px "
+        >
+          Bekräfta
+        </Elements.Buttons.SubmitButton>
+      </ButtonContainer>
+    </Container>
+  )
+}
+
+export default Component

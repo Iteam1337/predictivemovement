466e0fd0b91e84381830cd9d7f578129a90cda33
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-02-03 16:30:57 +0100

    chore: total distance and total duration

--- packages/engine-ui/src/components/DeliveryDetails.tsx
+++ packages/engine-ui/src/components/DeliveryDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as Elements from '../shared-elements'
 import styled from 'styled-components'
+import * as stores from '../utils/state/stores'
 
 const Wrapper = styled.div`
   width: 290px;
@@ -8,40 +9,84 @@ const Wrapper = styled.div`
   padding-left: 2rem;
 `
 
-const DeliveryDetails: React.FC<{ distance: string; duration: string }> = ({
-  distance,
-  duration,
-}) => {
+const DeliveryDetails: React.FC<{
+  distance: string
+  duration: string
+  bookingId: string
+}> = ({ distance, duration, bookingId }) => {
+  const transports = stores.dataState((state) => state.transports)
+
+  const transport = transports.find((transport) =>
+    transport.bookingIds
+      ? transport.bookingIds.map((id) => id === bookingId)
+      : null
+  )
+
+  const totalDistance = transport?.activities?.reduce(
+    (acc, activity) => acc + activity.distance,
+    0
+  )
+
+  const totalDuration = transport?.activities?.reduce(
+    (acc, activity) => acc + activity.duration,
+    0
+  )
+
   const getDistance = (distance: number) => {
-    return Math.round(distance / 1000)
+    const dist = Math.round(distance / 1000)
+
+    return `${dist} km`
   }
+
   const getDuration = (duration: number) => {
-    return Math.round(duration / 60)
+    const num = Math.round(duration / 60)
+    const hours = Math.floor(num / 60)
+    const minutes = num % 60
+    if (hours === 0) {
+      return `${minutes}min`
+    }
+    return `${hours}h ${minutes}min`
   }
+
   return (
     <Wrapper>
       <Elements.Layout.SectionWithMargin>
         <Elements.Typography.StrongParagraph>
           Statistik
         </Elements.Typography.StrongParagraph>
-        <Elements.Layout.FlexRowWrapper>
-          <Elements.Typography.InfoMd>
-            Total körsträcka
-          </Elements.Typography.InfoMd>
-          <Elements.Typography.InfoMd>
-            {getDistance(parseInt(distance))} km
-          </Elements.Typography.InfoMd>
-        </Elements.Layout.FlexRowWrapper>
+        {totalDistance && (
+          <Elements.Layout.FlexRowWrapper>
+            <Elements.Typography.InfoMd>
+              Total körsträcka
+            </Elements.Typography.InfoMd>
+            <Elements.Typography.InfoMd>
+              {getDistance(totalDistance)}
+            </Elements.Typography.InfoMd>
+          </Elements.Layout.FlexRowWrapper>
+        )}
         <Elements.Layout.FlexRowWrapper>
           <Elements.Typography.InfoMd>
             Del av körsträcka
           </Elements.Typography.InfoMd>
-          <Elements.Typography.InfoMd>17,5 km</Elements.Typography.InfoMd>
-        </Elements.Layout.FlexRowWrapper>
-        <Elements.Layout.FlexRowWrapper>
-          <Elements.Typography.InfoMd>Total körtid</Elements.Typography.InfoMd>
           <Elements.Typography.InfoMd>
-            {getDuration(parseInt(duration))} min
+            {getDistance(parseInt(distance))}
+          </Elements.Typography.InfoMd>
+        </Elements.Layout.FlexRowWrapper>
+
+        {totalDuration && (
+          <Elements.Layout.FlexRowWrapper>
+            <Elements.Typography.InfoMd>
+              Total körtid
+            </Elements.Typography.InfoMd>
+            <Elements.Typography.InfoMd>
+              {getDuration(totalDuration)}
+            </Elements.Typography.InfoMd>
+          </Elements.Layout.FlexRowWrapper>
+        )}
+        <Elements.Layout.FlexRowWrapper>
+          <Elements.Typography.InfoMd>Del av körtid</Elements.Typography.InfoMd>
+          <Elements.Typography.InfoMd>
+            {getDuration(parseInt(duration))}
           </Elements.Typography.InfoMd>
         </Elements.Layout.FlexRowWrapper>
       </Elements.Layout.SectionWithMargin>

d5edaaf31f8e27c0bceb365c471b8db7d2da1f0a
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-02-19 11:33:18 +0100

    chore: sort transports alphabetically

--- packages/engine-ui/src/components/TransportsList.tsx
+++ packages/engine-ui/src/components/TransportsList.tsx
@@ -3,11 +3,13 @@ import * as stores from '../utils/state/stores'
 import * as Elements from '../shared-elements/'
 import * as helpers from '../utils/helpers'
 import { Transport } from '../types'
+import { Link, useRouteMatch } from 'react-router-dom'
 
 const TransportsList: React.FC<{ transports: Transport[] }> = ({
   transports,
 }) => {
   const setUIState = stores.ui((state) => state.dispatch)
+  const { url } = useRouteMatch()
 
   if (!transports.length)
     return (
@@ -27,43 +29,61 @@ const TransportsList: React.FC<{ transports: Transport[] }> = ({
     return null
   })
 
+  const sortedTransports = transports.sort((a, b) =>
+    a.metadata.profile.localeCompare(b.metadata.profile)
+  )
+
   return (
     <Elements.Layout.TransportsList>
-      {sortedFleets.map((fleet) => {
-        return (
-          <div>
-            <h3>{fleet}</h3>
+      {sortedFleets &&
+        sortedFleets.map((fleet) => {
+          return (
+            <div>
+              <h3>
+                {fleet === '' && sortedFleets.length === 1
+                  ? 'Aktuella Transporter'
+                  : fleet === '' && sortedFleets.length > 1
+                  ? 'Övriga Transporter'
+                  : fleet}
+              </h3>
 
-            {transports.map((transport) => {
-              if (transport.metadata.fleet === fleet) {
-                return (
-                  <li key={transport.id}>
-                    <Elements.Links.RoundedLink
-                      color={transport.color}
-                      onMouseOver={() =>
-                        setUIState({
-                          type: 'highlightTransport',
-                          payload: transport.id,
-                        })
-                      }
-                      onMouseLeave={() =>
-                        setUIState({
-                          type: 'highlightTransport',
-                          payload: undefined,
-                        })
-                      }
-                      to={`/transports/${transport.id}`}
-                    >
-                      {transport.metadata?.profile?.toUpperCase() ||
-                        helpers.getLastFourChars(transport.id).toUpperCase()}
-                    </Elements.Links.RoundedLink>
-                  </li>
-                )
-              } else return null
-            })}
-          </div>
-        )
-      })}
+              {sortedTransports.map((transport) => {
+                if (transport.metadata.fleet === fleet) {
+                  return (
+                    <li key={transport.id}>
+                      <Elements.Links.RoundedLink
+                        color={transport.color}
+                        onMouseOver={() =>
+                          setUIState({
+                            type: 'highlightTransport',
+                            payload: transport.id,
+                          })
+                        }
+                        onMouseLeave={() =>
+                          setUIState({
+                            type: 'highlightTransport',
+                            payload: undefined,
+                          })
+                        }
+                        to={`/transports/${transport.id}`}
+                      >
+                        {transport.metadata?.profile?.toUpperCase() ||
+                          helpers.getLastFourChars(transport.id).toUpperCase()}
+                      </Elements.Links.RoundedLink>
+                    </li>
+                  )
+                } else return null
+              })}
+              <Elements.Layout.FlexRowInCenter>
+                <Link to={`${url}/add-transport`}>
+                  <Elements.Buttons.SubmitButton color="#666666">
+                    + Lägg till transport
+                  </Elements.Buttons.SubmitButton>
+                </Link>
+              </Elements.Layout.FlexRowInCenter>
+            </div>
+          )
+        })}
     </Elements.Layout.TransportsList>
   )
 }

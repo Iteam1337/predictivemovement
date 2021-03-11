fbcfa54104ec9f23aa4677ae73cc338c757049fe
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-03-11 11:12:11 +0100

    chore: sort booking by fleet

--- packages/engine-ui/src/components/History.tsx
+++ packages/engine-ui/src/components/History.tsx
@@ -3,32 +3,90 @@ import * as Elements from '../shared-elements'
 import * as stores from '../utils/state/stores'
 import * as helpers from '../utils/helpers'
 import styled from 'styled-components'
+
 const moment = require('moment')
 
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(10, 100px);
-  /* grid-auto-rows: 50px; */
-  /* grid-template-rows: minmax(auto, auto); */
   width: 1000px;
 `
 
-const GridItemColumn = styled.div<{ place: number }>`
+const GridItemColumnTitle = styled.div<{ place: number }>`
   justify-self: center;
   grid-column: ${(p) => p.place} / span 1;
   height: 50px;
 `
 
-const GridItemRow = styled.div<{ place: number }>`
+const GridItemColumn = styled.div<{ place: number }>`
   justify-self: center;
   height: 4rem;
   grid-row: ${(p) => p.place} / span 1;
   font-size: 12px;
 `
 
+const FleetInput = styled.div`
+  background-color: #f1f3f5;
+  border-radius: 0.25rem;
+  width: 200px;
+  font-size: 0.875rem;
+  :focus {
+    outline-color: #13c57b;
+  }
+`
+
+const DropdownButton = styled.button`
+  width: inherit;
+  text-align: left;
+  padding: 0.5rem;
+  border: 1px solid grey;
+  :focus {
+    outline-color: #13c57b;
+  }
+  background-color: #f1f3f5;
+`
+
+const FleetText = styled.p`
+  padding: 0.55rem;
+  margin: 0;
+`
+
+const DropDown: React.FC<{
+  items: string[]
+  handleDropdownSelect: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    value: string
+  ) => void
+}> = ({ items, handleDropdownSelect }) => {
+  return (
+    <>
+      {items.map((item, i) => {
+        return (
+          <DropdownButton
+            key={i}
+            onMouseDown={(event) => handleDropdownSelect(event, item)}
+          >
+            {item}
+          </DropdownButton>
+        )
+      })}
+    </>
+  )
+}
+
 const History: React.FC<{}> = () => {
   const transports = stores.dataState((state) => state.transports)
   const bookings = stores.dataState((state) => state.bookings)
+  const [showDropdown, setShowDropdown] = React.useState(false)
+  const [selectedFleet, setSelectedFleet] = React.useState('Alla')
+  const handleDropdownSelect = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    item: string
+  ) => {
+    event.persist()
+    setShowDropdown(false)
+    setSelectedFleet(item)
+  }
 
   const deliveredBookings = bookings.filter(
     (booking) => booking.status === 'delivered'
@@ -47,49 +105,117 @@ const History: React.FC<{}> = () => {
     { title: 'Andel av rutt', place: 10 },
   ]
 
-  //   const formatTime = (date) => moment(date).format('HH:mm')
-  //   const formatDate = (date) => moment(date).format('YYYY-MM-DD')
+  const formatTime = (date: string) => moment(date).format('HH:mm')
+  const formatDate = (date: string) => moment(date).format('YYYY-MM-DD')
+
+  const getDistance = (distance: number) => {
+    return Math.round(distance / 1000)
+  }
+
+  const deliveredBookingsWithTransport = deliveredBookings.map((booking) => {
+    const transport = transports.find((t) =>
+      t.bookingIds?.find((id) => id === booking.id)
+    )
+
+    return {
+      bookingId: booking.id,
+      fleet: (transport && transport.metadata.fleet) || 'Saknas',
+      transport: (transport && transport.id) || 'Saknas',
+      customer: 'kund',
+      day:
+        booking.events.find((b) => b.type === 'delivered')?.timestamp ||
+        'Saknas',
+      time:
+        booking.events.find((b) => b.type === 'delivered')?.timestamp ||
+        'Saknas',
+      weight: booking.size.weight,
+      dimensions: 'dimentioner',
+      distance: booking.route.distance,
+      routePercentage: '100%',
+    }
+  })
+
+  const fleets = deliveredBookingsWithTransport.map((booking) => booking.fleet)
+  const uniqueFleets = fleets.filter(
+    (item, index) => fleets.indexOf(item) === index
+  )
+
+  const selectedBookings = deliveredBookingsWithTransport.filter((booking) =>
+    selectedFleet === 'Alla' ? booking.fleet : booking.fleet === selectedFleet
+  )
 
   return (
     <div style={{ width: '100vw' }}>
+      <p>Levererade bokningar</p>
+
+      <div
+        style={{
+          display: 'flex',
+          width: '50%',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div style={{ width: '200px' }}>
+          <p>Flotta </p>
+          <FleetInput
+            onBlur={() => setShowDropdown(false)}
+            onClick={() => setShowDropdown(true)}
+          >
+            <FleetText>{selectedFleet}</FleetText>
+            {showDropdown && (
+              <DropDown
+                items={['Alla', ...uniqueFleets]}
+                handleDropdownSelect={handleDropdownSelect}
+              />
+            )}
+          </FleetInput>
+        </div>
+
+        <p>Fr.o.m. datum</p>
+        <p>T.o.m. datum</p>
+      </div>
       <div style={{ width: '1000px', marginTop: '7rem' }}>
         <GridContainer>
-          {titles.map((titel) => (
-            <GridItemColumn place={titel.place}>{titel.title}</GridItemColumn>
+          {titles.map((titel, i) => (
+            <GridItemColumnTitle key={i} place={titel.place}>
+              {titel.title}
+            </GridItemColumnTitle>
           ))}
-          {deliveredBookings.map((booking, i) => {
-            // try to get booking.event in types.
-
+          {selectedBookings.map((booking, i) => {
             return (
               <>
-                <GridItemRow place={i + 2}>
+                <GridItemColumn place={i + 2}>
                   <Elements.Layout.InlineContainer>
-                    <Elements.Links.RoundedLink to={`/bookings/${booking.id}`}>
-                      {helpers.getLastFourChars(booking.id).toUpperCase()}
+                    <Elements.Links.RoundedLink
+                      to={`/bookings/${booking.bookingId}`}
+                    >
+                      {helpers
+                        .getLastFourChars(booking.bookingId)
+                        .toUpperCase()}
                     </Elements.Links.RoundedLink>
                   </Elements.Layout.InlineContainer>
-                </GridItemRow>
-                <GridItemRow place={i + 2}>
-                  {transports.map((t) =>
-                    t.bookingIds?.map((a) =>
-                      a === booking.id ? t.metadata.fleet : null
-                    )
-                  )}
-                </GridItemRow>
-                <GridItemRow place={i + 2}>
-                  {transports.map((t) =>
-                    t.bookingIds?.map((a) => (a === booking.id ? t.id : null))
-                  )}
-                </GridItemRow>
-                <GridItemRow place={i + 2}>kund</GridItemRow>
-                <GridItemRow place={i + 2}>hej</GridItemRow>
-                <GridItemRow place={i + 2}>tid</GridItemRow>
-                <GridItemRow place={i + 2}>
-                  {booking.size.weight ?? ''}
-                </GridItemRow>
-                <GridItemRow place={i + 2}>dimentioner</GridItemRow>
-                <GridItemRow place={i + 2}>sträcka</GridItemRow>
-                <GridItemRow place={i + 2}>andel av rutt</GridItemRow>
+                </GridItemColumn>
+                <GridItemColumn place={i + 2}>{booking.fleet}</GridItemColumn>
+                <GridItemColumn place={i + 2}>
+                  {booking.transport}
+                </GridItemColumn>
+                <GridItemColumn place={i + 2}>kund</GridItemColumn>
+                <GridItemColumn place={i + 2}>
+                  {formatDate(booking.time)}
+                </GridItemColumn>
+                <GridItemColumn place={i + 2}>
+                  {formatTime(booking.time)}
+                </GridItemColumn>
+                <GridItemColumn place={i + 2}>{booking.weight}</GridItemColumn>
+                <GridItemColumn place={i + 2}>
+                  {booking.dimensions}
+                </GridItemColumn>
+                <GridItemColumn place={i + 2}>
+                  {getDistance(booking.distance)} km
+                </GridItemColumn>
+                <GridItemColumn place={i + 2}>
+                  {booking.routePercentage}
+                </GridItemColumn>
               </>
             )
           })}

a4c8680f14a4b5518d3adf7860be3c8c2d7e73f6
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2021-03-12 10:15:05 +0100

    chore: splitted up code into component

--- packages/engine-ui/src/components/History.tsx
+++ packages/engine-ui/src/components/History.tsx
@@ -3,90 +3,41 @@ import * as Elements from '../shared-elements'
 import * as stores from '../utils/state/stores'
 import * as helpers from '../utils/helpers'
 import styled from 'styled-components'
+import SortHistory from './SortHistory'
 
 const moment = require('moment')
 
 const GridContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(10, 100px);
-  width: 1000px;
+  grid-template-columns: repeat(10, fit-content(150px));
+  @media (max-width: 768px) {
+    background-color: coral;
+  }
 `
 
 const GridItemColumnTitle = styled.div<{ place: number }>`
   justify-self: center;
+  align-self: center;
   grid-column: ${(p) => p.place} / span 1;
-  height: 50px;
+  padding: 1rem;
 `
 
 const GridItemColumn = styled.div<{ place: number }>`
   justify-self: center;
-  height: 4rem;
+  align-self: center;
   grid-row: ${(p) => p.place} / span 1;
   font-size: 12px;
+  padding: 0.6rem 1rem;
 `
-
-const FleetInput = styled.div`
-  background-color: #f1f3f5;
-  border-radius: 0.25rem;
-  width: 200px;
-  font-size: 0.875rem;
-  :focus {
-    outline-color: #13c57b;
-  }
-`
-
-const DropdownButton = styled.button`
-  width: inherit;
-  text-align: left;
-  padding: 0.5rem;
-  border: 1px solid grey;
-  :focus {
-    outline-color: #13c57b;
-  }
-  background-color: #f1f3f5;
-`
-
-const FleetText = styled.p`
-  padding: 0.55rem;
-  margin: 0;
-`
-
-const DropDown: React.FC<{
-  items: string[]
-  handleDropdownSelect: (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    value: string
-  ) => void
-}> = ({ items, handleDropdownSelect }) => {
-  return (
-    <>
-      {items.map((item, i) => {
-        return (
-          <DropdownButton
-            key={i}
-            onMouseDown={(event) => handleDropdownSelect(event, item)}
-          >
-            {item}
-          </DropdownButton>
-        )
-      })}
-    </>
-  )
-}
 
 const History: React.FC<{}> = () => {
   const transports = stores.dataState((state) => state.transports)
   const bookings = stores.dataState((state) => state.bookings)
-  const [showDropdown, setShowDropdown] = React.useState(false)
   const [selectedFleet, setSelectedFleet] = React.useState('Alla')
-  const handleDropdownSelect = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    item: string
-  ) => {
-    event.persist()
-    setShowDropdown(false)
-    setSelectedFleet(item)
-  }
+  const today = moment().format('YYYY-MM-DD')
+  const sevenDaysAgo = moment().subtract(7, 'days').format('YYYY-MM-DD')
+  const [fromDate, setFromDate] = React.useState(sevenDaysAgo)
+  const [toDate, setToDate] = React.useState(today)
 
   const deliveredBookings = bookings.filter(
     (booking) => booking.status === 'delivered'
@@ -140,86 +91,79 @@ const History: React.FC<{}> = () => {
     (item, index) => fleets.indexOf(item) === index
   )
 
-  const selectedBookings = deliveredBookingsWithTransport.filter((booking) =>
-    selectedFleet === 'Alla' ? booking.fleet : booking.fleet === selectedFleet
+  const sortedBookings = deliveredBookingsWithTransport.filter((booking) =>
+    booking.day <= toDate && booking.day >= fromDate
+      ? selectedFleet === 'Alla'
+        ? booking.fleet
+        : booking.fleet === selectedFleet
+      : null
   )
 
   return (
     <div style={{ width: '100vw' }}>
       <p>Levererade bokningar</p>
-
-      <div
-        style={{
-          display: 'flex',
-          width: '50%',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div style={{ width: '200px' }}>
-          <p>Flotta </p>
-          <FleetInput
-            onBlur={() => setShowDropdown(false)}
-            onClick={() => setShowDropdown(true)}
-          >
-            <FleetText>{selectedFleet}</FleetText>
-            {showDropdown && (
-              <DropDown
-                items={['Alla', ...uniqueFleets]}
-                handleDropdownSelect={handleDropdownSelect}
-              />
-            )}
-          </FleetInput>
-        </div>
-
-        <p>Fr.o.m. datum</p>
-        <p>T.o.m. datum</p>
-      </div>
-      <div style={{ width: '1000px', marginTop: '7rem' }}>
-        <GridContainer>
-          {titles.map((titel, i) => (
-            <GridItemColumnTitle key={i} place={titel.place}>
-              {titel.title}
-            </GridItemColumnTitle>
-          ))}
-          {selectedBookings.map((booking, i) => {
-            return (
-              <>
-                <GridItemColumn place={i + 2}>
-                  <Elements.Layout.InlineContainer>
-                    <Elements.Links.RoundedLink
-                      to={`/bookings/${booking.bookingId}`}
-                    >
-                      {helpers
-                        .getLastFourChars(booking.bookingId)
-                        .toUpperCase()}
-                    </Elements.Links.RoundedLink>
-                  </Elements.Layout.InlineContainer>
-                </GridItemColumn>
-                <GridItemColumn place={i + 2}>{booking.fleet}</GridItemColumn>
-                <GridItemColumn place={i + 2}>
-                  {booking.transport}
-                </GridItemColumn>
-                <GridItemColumn place={i + 2}>kund</GridItemColumn>
-                <GridItemColumn place={i + 2}>
-                  {formatDate(booking.time)}
-                </GridItemColumn>
-                <GridItemColumn place={i + 2}>
-                  {formatTime(booking.time)}
-                </GridItemColumn>
-                <GridItemColumn place={i + 2}>{booking.weight}</GridItemColumn>
-                <GridItemColumn place={i + 2}>
-                  {booking.dimensions}
-                </GridItemColumn>
-                <GridItemColumn place={i + 2}>
-                  {getDistance(booking.distance)} km
-                </GridItemColumn>
-                <GridItemColumn place={i + 2}>
-                  {booking.routePercentage}
-                </GridItemColumn>
-              </>
-            )
-          })}
-        </GridContainer>
+      <SortHistory
+        setFromDate={setFromDate}
+        setToDate={setToDate}
+        setSelectedFleet={setSelectedFleet}
+        selectedFleet={selectedFleet}
+        uniqueFleets={uniqueFleets}
+        fromDate={fromDate}
+        toDate={toDate}
+      />
+      <div>
+        {sortedBookings.length === 0 && (
+          <p>Inga levererade bokningar mellan dessa datum.</p>
+        )}
+        {sortedBookings.length > 0 && (
+          <GridContainer>
+            {titles.map((titel, i) => (
+              <GridItemColumnTitle key={i} place={titel.place}>
+                {titel.title}
+              </GridItemColumnTitle>
+            ))}
+            {sortedBookings.map((booking, i) => {
+              return (
+                <>
+                  <GridItemColumn place={i + 2}>
+                    <Elements.Layout.InlineContainer>
+                      <Elements.Links.RoundedLink
+                        to={`/bookings/${booking.bookingId}`}
+                      >
+                        {helpers
+                          .getLastFourChars(booking.bookingId)
+                          .toUpperCase()}
+                      </Elements.Links.RoundedLink>
+                    </Elements.Layout.InlineContainer>
+                  </GridItemColumn>
+                  <GridItemColumn place={i + 2}>{booking.fleet}</GridItemColumn>
+                  <GridItemColumn place={i + 2}>
+                    {booking.transport}
+                  </GridItemColumn>
+                  <GridItemColumn place={i + 2}>kund</GridItemColumn>
+                  <GridItemColumn place={i + 2}>
+                    {formatDate(booking.day)}
+                  </GridItemColumn>
+                  <GridItemColumn place={i + 2}>
+                    {formatTime(booking.time)}
+                  </GridItemColumn>
+                  <GridItemColumn place={i + 2}>
+                    {booking.weight}
+                  </GridItemColumn>
+                  <GridItemColumn place={i + 2}>
+                    {booking.dimensions}
+                  </GridItemColumn>
+                  <GridItemColumn place={i + 2}>
+                    {getDistance(booking.distance)} km
+                  </GridItemColumn>
+                  <GridItemColumn place={i + 2}>
+                    {booking.routePercentage}
+                  </GridItemColumn>
+                </>
+              )
+            })}
+          </GridContainer>
+        )}
       </div>
     </div>
   )
---
+++ packages/engine-ui/src/components/SortHistory.tsx
@@ -0,0 +1,177 @@
+import React from 'react'
+import { Dispatch, SetStateAction } from 'react'
+import styled from 'styled-components'
+import * as Elements from '../shared-elements'
+import arrowIcon from '../assets/input-arrowdown.svg'
+
+const FleetInput = styled.div`
+  background-color: #f1f3f5;
+  border-radius: 0.25rem;
+  width: 150px;
+  font-size: 0.875rem;
+  :focus {
+    outline-color: #13c57b;
+  }
+  @media (max-width: 768px) {
+    width: 80vw;
+  }
+`
+
+const FleetText = styled.p`
+  padding: 0.55rem;
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Icon = styled.img`
+  width: 10px;
+  height: 10px;
+  margin-top: 5px;
+`
+
+const Wrapper = styled.div`
+  width: 8rem;
+`
+
+const SortWrapper = styled.div`
+  display: flex;
+  width: 50%;
+  justify-content: space-between;
+  margin-bottom: 4rem;
+
+  @media (max-width: 768px) {
+    margin-bottom: 2rem;
+    width: 100%;
+    flex-direction: column;
+  }
+`
+const DropdownButton = styled.button`
+  width: inherit;
+  text-align: left;
+  padding: 0.5rem;
+  border: 1px solid grey;
+  :focus {
+    outline-color: #13c57b;
+  }
+  background-color: #f1f3f5;
+`
+
+const DropDown: React.FC<{
+  items: string[]
+  handleDropdownSelect: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    value: string
+  ) => void
+}> = ({ items, handleDropdownSelect }) => {
+  return (
+    <>
+      {items.map((item, i) => {
+        return (
+          <DropdownButton
+            key={i}
+            onMouseDown={(event) => handleDropdownSelect(event, item)}
+          >
+            {item}
+          </DropdownButton>
+        )
+      })}
+    </>
+  )
+}
+
+const SortHistory: React.FC<{
+  setFromDate: Dispatch<SetStateAction<string>>
+  setToDate: Dispatch<SetStateAction<string>>
+  setSelectedFleet: Dispatch<SetStateAction<string>>
+  selectedFleet: string
+  uniqueFleets: string[]
+  fromDate: string
+  toDate: string
+}> = ({
+  setFromDate,
+  setToDate,
+  setSelectedFleet,
+  selectedFleet,
+  uniqueFleets,
+  fromDate,
+  toDate,
+}) => {
+  const dropdownFleets = React.useRef<HTMLDivElement>(null)
+  const [showDropdown, setShowDropdown] = React.useState(false)
+
+  const updateFromDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFromDate(e.currentTarget.value)
+  }
+
+  const updateToDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setToDate(e.currentTarget.value)
+  }
+
+  const handleDropdownSelect = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    item: string
+  ) => {
+    event.persist()
+    setShowDropdown(false)
+    setSelectedFleet(item)
+  }
+
+  React.useEffect(() => {
+    document.addEventListener('click', handleDocumentClick)
+    return () => document.removeEventListener('click', handleDocumentClick)
+  }, [])
+
+  const handleDocumentClick = (e: any) => {
+    if (e.target && !dropdownFleets.current?.contains(e.target as Node)) {
+      setShowDropdown(false)
+    }
+  }
+
+  return (
+    <SortWrapper>
+      <Wrapper>
+        <p>Flotta </p>
+        <FleetInput
+          onClick={() => setShowDropdown((current) => !current)}
+          ref={dropdownFleets}
+        >
+          <FleetText>
+            {selectedFleet} <Icon src={arrowIcon} />
+          </FleetText>
+
+          {showDropdown && (
+            <DropDown
+              items={['Alla', ...uniqueFleets]}
+              handleDropdownSelect={handleDropdownSelect}
+            />
+          )}
+        </FleetInput>
+      </Wrapper>
+
+      <Wrapper>
+        <p>Fr.o.m. datum</p>
+        <FleetInput>
+          <Elements.Form.TextInput
+            type="date"
+            value={fromDate}
+            onChange={updateFromDate}
+          />
+        </FleetInput>
+      </Wrapper>
+
+      <Wrapper>
+        <p>T.o.m. datum</p>
+        <FleetInput>
+          <Elements.Form.TextInput
+            type="date"
+            value={toDate}
+            onChange={updateToDate}
+          />
+        </FleetInput>
+      </Wrapper>
+    </SortWrapper>
+  )
+}
+
+export default SortHistory

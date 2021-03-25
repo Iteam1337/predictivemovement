daefd9724cf3473eedb05c7c276962faa5d893d2
izabellalarsson <izzwii.izabella@gmail.com>
2021-03-25 11:24:50 +0100

    chore: clean up

--- packages/engine-ui/src/components/forms/validation/index.ts
+++ packages/engine-ui/src/components/forms/validation/index.ts
@@ -2,10 +2,13 @@ import phone from 'phone'
 
 export const validatePhoneNumber = (value: string) => {
   let error
-  if (value == '-') return
+  if (value === '-') return
   const normalizedPhoneNumber = phone(value, 'SWE')
   if (normalizedPhoneNumber.length <= 0) {
     error = 'Fyll i ett korrekt telefonnummer'
+  }
+  if (value === '') {
+    error = validateNotEmpty(value)
   }
   return error
 }

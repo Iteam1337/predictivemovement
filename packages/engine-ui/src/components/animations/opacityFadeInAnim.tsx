3d9f68472e34689645583368e18c0320697456c2
Mikaela <54809602+mikaelaalu@users.noreply.github.com>
2021-03-29 09:06:51 +0200

    Chore/reduce fields (#350)
    * reduce fields and started on animations on bookingForm
    
    * rename animation and add styling to buttons
    
    * update animation file to tsx
    
    * Update telegram to only show phone number when phone number is added

---
+++ packages/engine-ui/src/components/animations/opacityFadeInAnim.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Transition } from 'react-transition-group'
+import styled from 'styled-components'
+
+const Animation = styled.div<{ state: string }>`
+  transition: opacity 500ms, transform 500ms;
+  opacity: ${({ state }) => (state === 'entered' ? 1 : 0)};
+  transform: ${({ state }) =>
+    state === 'entered' ? 'translateY(0)' : 'translateY(20%)'};
+`
+
+const OpacityFadeInAnim: React.FC<{ animate: boolean }> = ({
+  animate,
+  children,
+}) => {
+  return (
+    <Transition timeout={0} in={animate}>
+      {(state) => <Animation state={state}>{children}</Animation>}
+    </Transition>
+  )
+}
+
+export default OpacityFadeInAnim

6464b274b7aba08b9b3710eac99931d31dd3814a
izabellalarsson <izzwii.izabella@gmail.com>
2021-05-05 11:40:50 +0200

    chore: change layout for transport links

--- packages/engine-ui/src/shared-elements/links.ts
+++ packages/engine-ui/src/shared-elements/links.ts
@@ -1,6 +1,48 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
+const LinkWithDot = styled(Link)<{ dotColor: string }>`
+  ${({ dotColor }) =>
+    dotColor &&
+    `
+  &::before {
+    border-radius: 50%;
+    background-color: ${dotColor};
+    width: 12px;
+    height: 12px;
+    display: inline-block;
+    content: '';
+    margin-right: 5px;
+  }`}
+
+  opacity: 1;
+  padding: 0.5rem 0.6rem;
+  text-decoration: none;
+  display: inline-block;
+  font-weight: 500;
+  width: fit-content;
+  color: black;
+  font-family: 'Roboto Mono', monospace;
+  letter-spacing: 0.1rem;
+
+  :hover {
+    opacity: 0.8;
+  }
+`
+
+const PlaneLink = styled(Link)`
+  text-decoration: none;
+  opacity: 1;
+  color: #666666;
+  padding: 1rem 0.6rem;
+  display: block;
+  font-weight: bold;
+
+  :hover {
+    opacity: 0.9;
+  }
+`
+
 const RoundedLink = styled(Link)<{
   margin?: string
   color?: string
@@ -27,4 +69,4 @@ const RoundedLink = styled(Link)<{
   }
 `
 
-export { RoundedLink }
+export { RoundedLink, LinkWithDot, PlaneLink }

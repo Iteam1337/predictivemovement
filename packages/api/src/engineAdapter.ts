4f93881ea41a1bf5fa155f34a57342c187a19143
Radu Achim <radu.achim@iteam.se>
2021-06-07 11:59:53 +0200

    chore: "refactoring" code
    we made some types, removed generic stream and added a specific case for create
    
    Co-authored-by: Emil Ahlbäck <mull@users.noreply.github.com>
    Co-authored-by: Adam Näslund <iteadam@users.noreply.github.com>

--- packages/api/src/engineAdapter.ts
+++ packages/api/src/engineAdapter.ts
@@ -1,7 +1,6 @@
 import {
-  waitForBookingNotification,
-  bookingNotifications,
   publishCreateBooking,
+  waitForBookingCreated
 } from './rabbitmqConnector'
 import { components } from './__generated__/schema'
 import { nanoid } from 'nanoid'
@@ -9,7 +8,7 @@ import { nanoid } from 'nanoid'
 const createBooking = async (booking: components['schemas']['Booking']) => {
   booking.id = nanoid()
   await publishCreateBooking(booking)
-  return waitForBookingNotification(bookingNotifications, booking.id, 'new')
+  return waitForBookingCreated(booking.id)
 }
 
 export { createBooking }
@@ -19,7 +18,7 @@ export { createBooking }
 /*
 
 1. Api generates an ID and adds that to payload and sends it to Rabbit
-2. Engine will use that ID for spawning a Booking Process
+2. Engine will use that ID for spawning a Booking Process [a]
 3. pushes a message to Rabbit to booking_updates exchange with key "new"
 {
   id: 'pmb-123s',
@@ -27,6 +26,11 @@ export { createBooking }
   pickup: {},
   external_id: 'some ID that UI has sent'
 }
-4. API will filter on booking_updates queue for keys `new` and `error` and id `pmb-123s`
+4. API will filter on booking_updates queue for keys `new` and `error` and id `pmb-123s` [b]
 In case of errors in the Engine or the ID not being valid Engine will push the error to booking_updates exchange with key `error` and includes the id `pmb-123s` and error message
+5. Return the booking object with a link to it
+
+L8r IT DO the error cases
+[a] If there is already something with that ID, fail, or just return the existing object?!
+[b] With a timeout, if it times out return the "request ID" to the caller and have them use the same id on any retries (to avoid duplicates)
 */
--- packages/api/src/rabbitmqConnector.ts
+++ packages/api/src/rabbitmqConnector.ts
@@ -2,14 +2,47 @@ const amqp = require('fluent-amqp')(process.env.AMQP_URL || 'amqp://localhost')
 import { components } from './__generated__/schema'
 import * as Highland from 'highland'
 
-const publishDeleteBooking = (bookingId: string) => {
-  return amqp
-    .exchange('incoming_booking_updates', 'topic', {
-      durable: true,
-    })
-    .publish(bookingId, 'deleted', {
-      persistent: true,
-    })
+type NewBookingEvent = {
+  id: string
+  /*
+     %{
+  assigned_to: nil,
+  delivery: %{
+    city: "Stockholm",
+    lat: 59.318951,
+    lon: 18.05167,
+    name: "Ringvägen 9, Stockholm",
+    street: "Ringvägen 9",
+    time_windows: [
+      %{
+        earliest: "2021-06-14T22:00:00.000Z",
+        latest: "2021-06-23T22:00:00.000Z"
+      } 
+    ]
+  },
+  events: [%{timestamp: ~U[2021-06-07 09:09:40.052627Z], type: :new}],
+  external_id: "POSTNORD nummber",
+  id: "pmb-zwq0nmnm",
+  metadata: "{\"cargo\":\"not bad stuff\",\"customer\":\"Zalando\",\"fragile\":false,\"recipient\":{\"contact\":\"+46708111111\",\"info\":\"Adam delivers package at this door\",\"name\":\"Adam the receiver\"},\"sender\":{\"contact\":\"+46708101010\",\"info\":\"Adam lives at different door\",\"name\":\"Adam the sender\"}}",
+  pickup: %{
+    city: "Stockholm",
+    lat: 59.270668,
+    lon: 18.045423,
+    name: "Fågelstavägen 26, Stockholm",
+    street: "Fågelstavägen 26",
+    time_windows: [
+      %{
+        earliest: "2021-06-07T22:00:00.000Z",
+        latest: "2021-06-09T22:00:00.000Z"
+      }
+    ]
+  },
+  requires_transport_id: nil,
+  route: "ROUTE BETWEEN PICKUP & DELIVERY",
+  size: %{measurements: [18, 18, 18], weight: 1}
+}
+
+    */
 }
 
 const publishCreateBooking = (booking: components['schemas']['Booking']) => {
@@ -25,56 +58,32 @@ const publishCreateBooking = (booking: components['schemas']['Booking']) => {
     )
 }
 
-const bookingNotifications = amqp
+const bookingNotificationStream = amqp
   .exchange('outgoing_booking_updates', 'topic', {
     durable: true,
   })
   .queue('booking_notifications.api', {
     durable: true,
   })
-  .subscribe({ noAck: true }, [
-    'new',
-    'picked_up',
-    'delivered',
-    'delivery_failed',
-    'deleted',
-  ])
-  .map((bookingRes: any) => {
-    const booking = bookingRes.json()
 
-    return {
-      ...booking,
-      status: bookingRes.fields.routingKey,
-    }
-  })
+const bookingCreatedStream = bookingNotificationStream
+  .subscribe({ noAck: true }, ['new'])
+  .map((res: any) => res.json())
 
-
-
-
-const waitForBookingNotification = (
-  stream: Highland.Stream<any>,
-  bookingId: string,
-  status: string
-) =>
-  stream
-    .fork() // do we fork here or do we expect the caller to have forked the stream before?
-    .filter(
-      (bookingNotification) => bookingNotification.bookingId === bookingId
-    )
-    .filter((bookingNotification) =>
-      [status, 'error'].includes(bookingNotification.status ?? '')
-    )
-    .tap(({ status, message }) => {
-      if (status === 'error') {
-        throw new Error(message)
-      }
-    })
-    .head() // Only pick the first
-    .toPromise(Promise)
-
-export {
-  publishDeleteBooking,
-  publishCreateBooking,
-  bookingNotifications,
-  waitForBookingNotification,
+function waitFirst<T>(
+  stream: Highland.Stream<T>,
+  predicate: (t: T) => boolean
+) {
+  return stream.filter(predicate).head()
 }
+
+function waitForBookingCreated(bookingId: string): Promise<NewBookingEvent> {
+  return waitFirst<NewBookingEvent>(
+    bookingCreatedStream,
+    ({ id }) => id === bookingId
+  )
+    .fork()
+    .toPromise(Promise)
+}
+
+export { publishCreateBooking, waitForBookingCreated }

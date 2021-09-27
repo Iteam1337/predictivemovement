7fe867a93d324b9b2e61372f4cbe9ab2bd867212
Adam Näslund <adam.naslund@iteam.se>
2021-09-27 16:29:46 +0200

    fix: update elixir version to 1.12

--- packages/engine_umbrella/apps/engine/mix.exs
+++ packages/engine_umbrella/apps/engine/mix.exs
@@ -5,7 +5,7 @@ defmodule Engine.MixProject do
     [
       app: :engine,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
